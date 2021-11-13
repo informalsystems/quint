e04c3b53c5ee1d9c021caa03476ac914f77bab45
Igor Konnov <igor@informal.systems>
2021-11-13 23:10:48 +0100

    add minimal CLI for parse

--- tntc/src/cli.ts
+++ tntc/src/cli.ts
@@ -1,3 +1,51 @@
 #!/usr/bin/env node
 
-console.log('tntc is expecting parameters')
+/**
+ * CLI for tntc.
+ *
+ * @author Igor Konnov, Informal Systems, 2021
+ */
+
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+import { cwd } from 'process'
+
+import { parsePhase1 } from './tntParserFrontend'
+
+import yargs from 'yargs/yargs'
+
+/**
+ * Parse a TNT specification.
+ */
+function parse (argv: any) {
+  const path = resolve(cwd(), argv.input)
+  const text = readFileSync(path).toString('utf8')
+  const result = parsePhase1(text)
+  if (result.kind === 'error') {
+    result.messages.forEach((m) => {
+      // TODO: use m.source instead of argv.input
+      const loc = `${argv.input}:${m.start.line + 1}:${m.start.col + 1}`
+      console.error(`${loc} - error ${m.explanation}`)
+    })
+  }
+}
+
+// construct parsing commands with yargs
+const parseCmd = {
+  command: 'parse <input>',
+  desc: 'parse a TNT specification',
+  builder: (yargs: any) => yargs
+    .option('out', {
+      desc: 'output file',
+      type: 'string'
+    })
+    .option('source-map', {
+      desc: 'name of the source map',
+      type: 'string'
+    }),
+  handler: parse
+}
+
+yargs(process.argv.slice(2))
+  .command(parseCmd)
+  .parse()
