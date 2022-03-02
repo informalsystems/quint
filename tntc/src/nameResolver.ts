43b3330fd3f50b09c0de87c0a07fac43de200275
gabrielamafra <gabrielamoreiramafra@gmail.com>
2022-03-02 14:16:58 -0300

    Check scope when resolving names

--- tntc/src/nameResolver.ts
+++ tntc/src/nameResolver.ts
@@ -14,7 +14,7 @@ export type NameResolutionResult =
 export function resolveNames (tntModule: TntModule, definitions: NameDefinition[]): NameResolutionResult {
   const results: NameResolutionResult[] = tntModule.defs.map(def => {
     // TODO: include definition name in result for error reporting
-    return def.kind === 'def' ? checkNamesInExpr(definitions, def.expr) : { kind: 'ok' }
+    return def.kind === 'def' ? checkNamesInExpr(definitions, def.expr, [def.expr.id]) : { kind: 'ok' }
   })
 
   if (results.some(result => result.kind === 'error')) {
@@ -29,18 +29,21 @@ export function resolveNames (tntModule: TntModule, definitions: NameDefinition[
   return { kind: 'ok' }
 }
 
-function checkNamesInExpr (nameDefinitions: NameDefinition[], expr: TntEx): NameResolutionResult {
+function checkNamesInExpr (nameDefinitions: NameDefinition[], expr: TntEx, scopes: BigInt[]): NameResolutionResult {
   switch (expr.kind) {
-    case 'name':
-      if (nameDefinitions.some(name => name.identifier === expr.name)) {
+    case 'name': {
+      const nameDefinitionsForScope = filterScope(nameDefinitions, scopes)
+
+      if (nameDefinitionsForScope.some(name => name.identifier === expr.name)) {
         return { kind: 'ok' }
       } else {
         return { kind: 'error', errors: [{ name: expr.name, expression: expr, trace: [] }] }
       }
+    }
 
     case 'app': {
       const errors = expr.args.flatMap(arg => {
-        const result = checkNamesInExpr(nameDefinitions, arg)
+        const result = checkNamesInExpr(nameDefinitions, arg, scopes.concat(arg.id))
 
         if (result.kind === 'error') {
           return result.errors.map(error => ({
@@ -56,7 +59,15 @@ function checkNamesInExpr (nameDefinitions: NameDefinition[], expr: TntEx): Name
       return errors.length === 0 ? { kind: 'ok' } : { kind: 'error', errors: errors }
     }
 
+    case 'lambda': return checkNamesInExpr(nameDefinitions, expr.expr, scopes.concat(expr.expr.id))
+
     default:
       return { kind: 'ok' }
   }
+}
+
+function filterScope (nameDefinitions: NameDefinition[], scopes: BigInt[]): NameDefinition[] {
+  return nameDefinitions.filter(definition => {
+    return !definition.scope || scopes.includes(definition.scope)
+  })
 }
