dfd6de9f13430f8c26609bee4978970e6c292cf0
gabrielamafra <gabrielamoreiramafra@gmail.com>
2022-03-21 15:27:58 -0300

    Add tests and comments for module collection

--- tntc/test/definitionsCollector.test.ts
+++ tntc/test/definitionsCollector.test.ts
@@ -55,13 +55,17 @@ describe('collectDefinitions', () => {
     })
 
     it('collects instances and scoped variables inside parameters', () => {
-      const tntModule = buildModuleWithDefs(['module test_module = test_module(a = val x = 10 {x})'])
+      const tntModule = buildModuleWithDefs([
+        'module test_module { def a = 2 }',
+        'module test_module_instance = test_module(a = val x = 10 {x})',
+      ])
 
       const result = collectDefinitions(tntModule)
 
       assert.includeDeepMembers(result.nameDefinitions, [
-        { kind: 'namespace', identifier: 'test_module' },
-        { kind: 'val', identifier: 'x', scope: BigInt(4) },
+        { kind: 'namespace', identifier: 'test_module_instance' },
+        { kind: 'def', identifier: 'test_module_instance::a' },
+        { kind: 'val', identifier: 'x', scope: BigInt(8) },
       ])
     })
 
@@ -71,7 +75,10 @@ describe('collectDefinitions', () => {
 
       const result = collectDefinitions(tntModule)
 
-      assert.deepInclude(result.nameDefinitions, { kind: 'namespace', identifier: 'test_module' })
+      assert.includeDeepMembers(result.nameDefinitions, [
+        { kind: 'namespace', identifier: 'test_module' },
+        { kind: 'def', identifier: 'test_module::a' },
+      ])
     })
 
     it('collects assume definitions and scoped variables in body', () => {
@@ -85,12 +92,26 @@ describe('collectDefinitions', () => {
       ])
     })
 
-    it('does not collect anything from imports', () => {
-      const tntModule = buildModuleWithDefs(['import test_module.*'])
+    it('collects imported module\'s definitions as unscoped definitions', () => {
+      const tntModule = buildModuleWithDefs([
+        'module test_module { def a = 1 }',
+        'import test_module.*',
+      ])
 
       const result = collectDefinitions(tntModule)
 
-      assert.deepEqual(result.nameDefinitions, defaultDefinitions)
+      assert.deepInclude(result.nameDefinitions, { kind: 'def', identifier: 'a' })
+    })
+
+    it('collects imported module\'s named definition as unscoped definition', () => {
+      const tntModule = buildModuleWithDefs([
+        'module test_module { def a = 1 }',
+        'import test_module.a',
+      ])
+
+      const result = collectDefinitions(tntModule)
+
+      assert.deepInclude(result.nameDefinitions, { kind: 'def', identifier: 'a' })
     })
   })
 
