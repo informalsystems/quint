a218d38925c40ea231415fc28553f305ad71ab9a
gabrielamafra <gabrielamoreiramafra@gmail.com>
2022-03-24 10:45:47 -0300

    Implement conflict scan for types and nested scopes

--- tntc/src/definitionsScanner.ts
+++ tntc/src/definitionsScanner.ts
@@ -1,4 +1,4 @@
-import { DefinitionTable, NameDefinition } from './definitionsCollector'
+import { DefinitionTable, NameDefinition, TypeDefinition } from './definitionsCollector'
 
 export interface Conflict {
     kind: 'operator' | 'type'
@@ -18,7 +18,16 @@ export interface ScopeTree {
 export function scanConflicts (table: DefinitionTable, tree: ScopeTree): DefinitionsConflictResult {
   const conflicts: Conflict[] = []
   const nameConflicts = table.nameDefinitions.reduce((conflicts: Map<string, BigInt[]>, def: NameDefinition) => {
-    const definitionsWithName = table.nameDefinitions.filter(d => d.identifier === def.identifier)
+    const definitionsWithName = table.nameDefinitions
+      .filter(d => d.identifier === def.identifier && (!d.scope || !def.scope || scopes(tree, d).includes(def.scope)))
+    if (definitionsWithName.length > 1) {
+      conflicts.set(def.identifier, definitionsWithName.map(d => d.reference ?? BigInt(0)))
+    }
+    return conflicts
+  }, new Map<string, BigInt[]>())
+
+  const typeConflicts = table.typeDefinitions.reduce((conflicts: Map<string, BigInt[]>, def: TypeDefinition) => {
+    const definitionsWithName = table.typeDefinitions.filter(d => d.identifier === def.identifier)
     if (definitionsWithName.length > 1) {
       conflicts.set(def.identifier, definitionsWithName.map(d => d.reference ?? BigInt(0)))
     }
@@ -26,6 +35,30 @@ export function scanConflicts (table: DefinitionTable, tree: ScopeTree): Definit
   }, new Map<string, BigInt[]>())
 
   nameConflicts.forEach((value, key) => (conflicts.push({ kind: 'operator', identifier: key, references: value })))
+  typeConflicts.forEach((value, key) => (conflicts.push({ kind: 'type', identifier: key, references: value })))
 
-  return { kind: 'error', conflicts: conflicts }
+  if (conflicts.length > 0) {
+    return { kind: 'error', conflicts: conflicts }
+  } else {
+    return { kind: 'ok' }
+  }
+}
+
+function scopes (treeNode: ScopeTree, definition: NameDefinition): BigInt[] {
+  if (treeNode.value === definition.scope) {
+    return [treeNode.value]
+  }
+
+  if (!treeNode.children) {
+    return []
+  }
+
+  return treeNode.children.flatMap(child => {
+    const childScopes = scopes(child, definition)
+    if (childScopes.length > 0) {
+      return childScopes.concat(treeNode.value)
+    }
+
+    return []
+  })
 }
