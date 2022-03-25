be4e4032e2803746e2b805b7eb91209f2918af9b
gabrielamafra <gabrielamoreiramafra@gmail.com>
2022-03-25 16:25:03 -0300

    Improve scope checks

--- tntc/src/definitionsCollector.ts
+++ tntc/src/definitionsCollector.ts
@@ -194,18 +194,16 @@ export function collectDefinitions (tntModule: TntModule): DefinitionTable {
         table.nameDefinitions.push(...def.overrides.flatMap(e => collectFromExpr(e[1])))
 
         // Alias definitions from the instanced module to the new name
-        const namespacedDefinitions = table.nameDefinitions.reduce((ds: NameDefinition[], d) => {
-          const names = d.identifier.split('::')
-          if (d.scope) {
-            // Don't collect scoped names
+        const namespacedDefinitions = table.nameDefinitions
+          .filter(d => !d.scope) // Don't copy scoped definitions
+          .reduce((ds: NameDefinition[], d) => {
+            const names = d.identifier.split('::')
+            // Collect this name scoped to the instance iff the import matches the module's namespace
+            if (names[0] === def.protoName && names[1]) {
+              ds.push({ kind: d.kind, identifier: `${def.name}::${names[1]}`, reference: def.id })
+            }
             return ds
-          }
-          // Collect this name scoped to the instance iff the import matches the module's namespace
-          if (names[0] === def.protoName && names[1]) {
-            ds.push({ kind: d.kind, identifier: `${def.name}::${names[1]}`, reference: def.id })
-          }
-          return ds
-        }, [])
+          }, [])
         table.nameDefinitions.push(...namespacedDefinitions)
         break
       }
@@ -217,26 +215,26 @@ export function collectDefinitions (tntModule: TntModule): DefinitionTable {
         })
         const moduleDefinitions = collectDefinitions(def.module)
         // Collect all definitions namespaced to module
-        const namespacedDefinitions = moduleDefinitions.nameDefinitions.filter(d => !d.scope).map(d => {
-          return { kind: d.kind, identifier: `${def.module.name}::${d.identifier}`, reference: def.id }
-        })
+        const namespacedDefinitions = moduleDefinitions.nameDefinitions
+          .filter(d => !d.scope) // Don't copy scoped definitions
+          .map(d => {
+            return { kind: d.kind, identifier: `${def.module.name}::${d.identifier}`, reference: def.id }
+          })
         table.nameDefinitions.push(...namespacedDefinitions)
         break
       }
       case 'import': {
         // FIXME: check if definitions are found, when we actually import them from other files
-        const namespacedDefinitions = table.nameDefinitions.reduce((ds: NameDefinition[], d) => {
-          if (d.scope) {
-            // Don't import scoped names
+        const namespacedDefinitions = table.nameDefinitions
+          .filter(d => !d.scope) // Don't copy scoped definitions
+          .reduce((ds: NameDefinition[], d) => {
+            const names = d.identifier.split('::')
+            // Collect this name as unscoped iff the import matches its namespace and name
+            if (names[0] === def.path && names[1] && (def.name === '*' || def.name === names[1])) {
+              ds.push({ kind: d.kind, identifier: names[1], reference: def.id })
+            }
             return ds
-          }
-          const names = d.identifier.split('::')
-          // Collect this name as unscoped iff the import matches its namespace and name
-          if (names[0] === def.path && names[1] && (def.name === '*' || def.name === names[1])) {
-            ds.push({ kind: d.kind, identifier: names[1], reference: def.id })
-          }
-          return ds
-        }, [])
+          }, [])
         table.nameDefinitions.push(...namespacedDefinitions)
         break
       }
