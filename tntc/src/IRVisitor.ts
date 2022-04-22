92b652c89422a1591f3a64b6fbaf77724c8fc0d1
gabrielamafra <gabrielamoreiramafra@gmail.com>
2022-04-22 11:39:32 -0300

    Use namespaced imports and add docs

--- tntc/src/IRVisitor.ts
+++ tntc/src/IRVisitor.ts
@@ -1,51 +1,79 @@
-import { TntEx, TntDef, TntModule, TntConst, TntName, TntApp, TntOpDef, TntModuleDef, TntInstance, TntImport, TntTypeDef, TntAssume, TntVar, TntInt, TntBool, TntLet, TntLambda, TntStr } from './tntIr'
-import { TntType, TntVarType, TntConstType, TntBoolType, TntIntType, TntStrType, TntSetType, TntSeqType, TntFunType, TntOperType, TntTupleType, TntRecordType, TntUnionType } from './tntTypes'
+/* ----------------------------------------------------------------------------------
+ * Copyright (c) Informal Systems 2022. All rights reserved.
+ * Licensed under the Apache 2.0.
+ * See License.txt in the project root for license information.
+ * --------------------------------------------------------------------------------- */
 
+/**
+ * Visitor pattern implementation for TNT IR components. Use this to navigate the IR instead
+ * of implementing a recursion over it yourself.
+ *
+ * @author Gabriela Mafra
+ *
+ * @module
+ */
+
+import * as ir from './tntIr'
+import * as t from './tntTypes'
+
+/**
+ * Interface to be implemented by visitor classes.
+ * Optionally defines functions for each IR component.
+ */
 export interface IRVisitor {
   /** General components */
-  visitExpr?: (expr: TntEx) => void
-  visitDef?: (def: TntDef) => void
-  visitType?: (type: TntType) => void
+  visitExpr?: (expr: ir.TntEx) => void
+  visitDef?: (def: ir.TntDef) => void
+  visitType?: (type: t.TntType) => void
 
   /** Definitions */
-  visitOpDef?: (def: TntOpDef) => void
-  visitConst?: (def: TntConst) => void
-  visitVar?: (def: TntVar) => void
-  visitAssume?: (def: TntAssume) => void
-  visitTypeDef?: (def: TntTypeDef) => void
-  visitImport?: (def: TntImport) => void
-  visitInstance?: (def: TntInstance) => void
-  visitModuleDef?: (def: TntModuleDef) => void
+  visitOpDef?: (def: ir.TntOpDef) => void
+  visitConst?: (def: ir.TntConst) => void
+  visitVar?: (def: ir.TntVar) => void
+  visitAssume?: (def: ir.TntAssume) => void
+  visitTypeDef?: (def: ir.TntTypeDef) => void
+  visitImport?: (def: ir.TntImport) => void
+  visitInstance?: (def: ir.TntInstance) => void
+  visitModuleDef?: (def: ir.TntModuleDef) => void
 
   /** Expressions */
-  visitName?: (expr: TntName) => void
-  visitBool?: (expr: TntBool) => void
-  visitInt?: (expr: TntInt) => void
-  visitStr?: (expr: TntStr) => void
-  visitApp?: (expr: TntApp) => void
-  visitLambda?: (expr: TntLambda) => void
-  visitLet?: (expr: TntLet) => void
+  visitName?: (expr: ir.TntName) => void
+  visitBool?: (expr: ir.TntBool) => void
+  visitInt?: (expr: ir.TntInt) => void
+  visitStr?: (expr: ir.TntStr) => void
+  visitApp?: (expr: ir.TntApp) => void
+  visitLambda?: (expr: ir.TntLambda) => void
+  visitLet?: (expr: ir.TntLet) => void
 
   /** Types */
-  visitBoolType?: (type: TntBoolType) => void
-  visitIntType?: (type: TntIntType) => void
-  visitStrType?: (type: TntStrType) => void
-  visitConstType?: (type: TntConstType) => void
-  visitVarType?: (type: TntVarType) => void
-  visitSetType?: (type: TntSetType) => void
-  visitSeqType?: (type: TntSeqType) => void
-  visitFunType?: (type: TntFunType) => void
-  visitOperType?: (type: TntOperType) => void
-  visitTupleType?: (type: TntTupleType) => void
-  visitRecordType?: (type: TntRecordType) => void
-  visitUnionType?: (type: TntUnionType) => void
+  visitBoolType?: (type: t.TntBoolType) => void
+  visitIntType?: (type: t.TntIntType) => void
+  visitStrType?: (type: t.TntStrType) => void
+  visitConstType?: (type: t.TntConstType) => void
+  visitVarType?: (type: t.TntVarType) => void
+  visitSetType?: (type: t.TntSetType) => void
+  visitSeqType?: (type: t.TntSeqType) => void
+  visitFunType?: (type: t.TntFunType) => void
+  visitOperType?: (type: t.TntOperType) => void
+  visitTupleType?: (type: t.TntTupleType) => void
+  visitRecordType?: (type: t.TntRecordType) => void
+  visitUnionType?: (type: t.TntUnionType) => void
 }
 
-export function walkModule (visitor: IRVisitor, tntModule: TntModule): void {
+/**
+ * Navigates a TNT module with a visitor, invoking the correspondent function for each
+ * found IR component.
+ *
+ * @param visitor: the IRVisitor instance with the functions to be invoked
+ * @param tntModule: the TNT module to be navigated
+ *
+ * @returns nothing, any collected information has to be a state inside the IRVisitor instance.
+ */
+export function walkModule (visitor: IRVisitor, tntModule: ir.TntModule): void {
   tntModule.defs.forEach(def => walkDefinition(visitor, def))
 }
 
-function walkDefinition (visitor: IRVisitor, def: TntDef) {
+function walkDefinition (visitor: IRVisitor, def: ir.TntDef) {
   if (visitor.visitDef) {
     visitor.visitDef(def)
   }
@@ -101,7 +129,7 @@ function walkDefinition (visitor: IRVisitor, def: TntDef) {
   }
 }
 
-function walkExpression (visitor: IRVisitor, expr: TntEx) {
+function walkExpression (visitor: IRVisitor, expr: ir.TntEx) {
   if (visitor.visitExpr) {
     visitor.visitExpr(expr)
   }
@@ -153,7 +181,7 @@ function walkExpression (visitor: IRVisitor, expr: TntEx) {
   }
 }
 
-function walkType (visitor: IRVisitor, type: TntType) {
+function walkType (visitor: IRVisitor, type: t.TntType) {
   if (visitor.visitType) {
     visitor.visitType(type)
   }
@@ -200,7 +228,7 @@ function walkType (visitor: IRVisitor, type: TntType) {
       if (visitor.visitFunType) {
         visitor.visitFunType(type)
       }
-      // Functions, check both argument and result
+      // Functions, walk both argument and result
       walkType(visitor, type.arg)
       walkType(visitor, type.res)
       break
@@ -209,7 +237,7 @@ function walkType (visitor: IRVisitor, type: TntType) {
       if (visitor.visitOperType) {
         visitor.visitOperType(type)
       }
-      // Operators, check all arguments and result
+      // Operators, walk all arguments and result
       type.args.forEach(arg => walkType(visitor, arg))
       walkType(visitor, type.res)
       break
@@ -218,7 +246,7 @@ function walkType (visitor: IRVisitor, type: TntType) {
       if (visitor.visitTupleType) {
         visitor.visitTupleType(type)
       }
-      // Tuples, check all elements
+      // Tuples, walk all elements
       type.elems.forEach(elem => walkType(visitor, elem))
       break
 
@@ -226,7 +254,7 @@ function walkType (visitor: IRVisitor, type: TntType) {
       if (visitor.visitRecordType) {
         visitor.visitRecordType(type)
       }
-      // Records, check all fields
+      // Records, walk all fields
       type.fields.forEach(field => walkType(visitor, field.fieldType))
       break
 
@@ -234,7 +262,7 @@ function walkType (visitor: IRVisitor, type: TntType) {
       if (visitor.visitUnionType) {
         visitor.visitUnionType(type)
       }
-      // Variants, check all fields for all records
+      // Variants, walk all fields for all records
       type.records.forEach(record => {
         record.fields.forEach(field => walkType(visitor, field.fieldType))
       })
