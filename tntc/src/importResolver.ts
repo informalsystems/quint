9fbbf6844b08c6606e2f65578ea3e49c166d0405
gabrielamafra <gabrielamoreiramafra@gmail.com>
2022-05-03 15:56:22 -0300

    Add docstrings and comments

--- tntc/src/importResolver.ts
+++ tntc/src/importResolver.ts
@@ -1,19 +1,52 @@
+/* ----------------------------------------------------------------------------------
+ * Copyright (c) Informal Systems 2022. All rights reserved.
+ * Licensed under the Apache 2.0.
+ * See License.txt in the project root for license information.
+ * --------------------------------------------------------------------------------- */
+
+/**
+ * Visits imports and instances copying definitions from modules being imported or instantiated
+ *
+ * @author Gabriela Mafra
+ *
+ * @module
+ */
+
 import { DefinitionTableByModule, DefinitionTable, emptyTable } from './definitionsCollector'
 import { TntImport, TntInstance, TntModule, TntModuleDef } from './tntIr'
 import { IRVisitor, walkModule } from './IRVisitor'
 
+/**
+ * A single import error
+ */
 export interface ImportError {
+  /* The name of the module supposed to be imported/instantiated */
   moduleName: string
+  /* The reference of the import/instance definition */
   reference: bigint
+  /* If importing a specific definition, the name of that definition */
   defName?: string
 }
 
+/**
+ * The result of import resolution for a TNT Module.
+ */
 export type ImportResolutionResult =
-  /* Success, all names were resolved */
+  /* Success, all imports were resolved and an updated table is provided */
   | { kind: 'ok', definitions: DefinitionTableByModule }
-  /* Error, at least one name couldn't be resolved. All errors are listed in errors */
+  /* Error, at least one import couldn't be resolved. All errors are listed in errors */
   | { kind: 'error', errors: ImportError[] }
 
+/**
+ * Explores the IR visiting all imports and instances. For each one, tries to find a definition
+ * table for the required module name, and if found, copies all unscoped non-default definitions
+ * to the current module, including a namespace in case of instances.
+ *
+ * @param tntModule the TNT module for which imports should be resolved
+ * @param definitions lists of names and type aliases collected for all modules
+ *
+ * @returns a successful result with updated definitions in case all imports were resolved, or the errors otherwise
+ */
 export function resolveImports (tntModule: TntModule, definitions: DefinitionTableByModule): ImportResolutionResult {
   const visitor = new ImportResolverVisitor(definitions)
   walkModule(visitor, tntModule)
@@ -54,18 +87,17 @@ class ImportResolverVisitor implements IRVisitor {
     }
     this.tables.set(def.name, copyTable(moduleTable))
 
-    // Alias definitions from the instanced module to the new name
     const namespacedDefinitions = moduleTable.valueDefinitions
       .filter(d => !d.scope)
       .map(d => {
         // FIXME: This identifier string manipulation should be replaced by a better representation, see #58
+        // Alias definitions from the instanced module to the new name
         return { kind: d.kind, identifier: `${def.name}::${d.identifier}`, reference: d.reference }
       })
     this.currentTable.valueDefinitions.push(...namespacedDefinitions)
   }
 
   enterImport (def: TntImport): void {
-    // FIXME: check if definitions are found, when we actually import them from other files
     const moduleTable = this.tables.get(def.path)
     if (!moduleTable) {
       this.errors.push({ moduleName: def.path, reference: def.id })
@@ -75,8 +107,10 @@ class ImportResolverVisitor implements IRVisitor {
     const importableDefinitions = moduleTable.valueDefinitions.filter(d => !d.scope && d.reference)
 
     if (def.name === '*') {
+      // Imports all definitions
       this.currentTable.valueDefinitions.push(...importableDefinitions)
     } else {
+      // Tries to find specific definition, reporting an error if not found
       const definition = importableDefinitions.find(d => d.identifier === def.name)
 
       if (definition) {
