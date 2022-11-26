438890d371ecbff00a39e40fbe0d02cf1ac5ea59
Shon Feder <shon@informal.systems>
2022-11-25 23:17:03 -0500

    Refactor CLI command pipelines
    - Use functional data streams
    - Make output writing composable

--- tntc/src/cli.ts
+++ tntc/src/cli.ts
@@ -9,14 +9,14 @@
  * @author Igor Konnov, Gabriela Moreira, Informal Systems, 2021-2022
  */
 
-import { PathLike, existsSync, readFileSync, writeFileSync } from 'fs'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
 import JSONbig from 'json-bigint'
 import lineColumn from 'line-column'
 import { resolve } from 'path'
 import { cwd } from 'process'
 
 import { formatError } from './errorReporter'
-import { ErrorMessage, ParserPhase2, compactSourceMap, parsePhase1, parsePhase2 } from './tntParserFrontend'
+import { ErrorMessage, Loc, compactSourceMap, parsePhase1, parsePhase2 } from './tntParserFrontend'
 
 import yargs from 'yargs/yargs'
 import { inferEffects } from './effects/inferrer'
@@ -25,125 +25,102 @@ import { errorTreeToString } from './errorTree'
 
 import { Either, left, right } from '@sweet-monads/either'
 import { Arguments } from 'yargs'
+import { Effect } from './effects/base'
 import { effectToString } from './effects/printing'
 import { tntRepl } from './repl'
+import { TntModule } from './tntIr'
+import { TypeScheme } from './types/base'
 import { inferTypes } from './types/inferrer'
 import { typeSchemeToString } from './types/printing'
+import { LookupTableByModule } from './lookupTable'
+
+export type status = 'loaded' | 'parsed' | 'typechecked'
+
+/** The data from a ProcedureStatus that may be output to --out */
+interface OutputStatus {
+  status: status,
+  warnings: any[], // TODO it doesn't look like this is being used for anything. Should we remove it?
+  module?: TntModule,
+  types?: Map<bigint, TypeScheme>,
+  effects?: Map<bigint, Effect>,
+}
+
+
+interface ProcedureStatus extends OutputStatus {
+  args: any,
+}
+
+interface LoadedStatus extends ProcedureStatus {
+  sourceCode: string
+}
+
+interface ParsedStatus extends LoadedStatus {
+  module: TntModule,
+  sourceMap: Map<bigint, Loc>,
+  table: LookupTableByModule,
+}
+
+interface TypecheckedStatus extends ParsedStatus {
+  types: Map<bigint, TypeScheme>,
+  effects: Map<bigint, Effect>,
+}
+
+/** Load a file into a string
+ *
+ * @param args the CLI arguments parsed by yargs */
+export function load(args: any): Either<string, LoadedStatus> {
+  if (existsSync(args.input)) {
+    try {
+      const sourceCode = readFileSync(args.input, 'utf8')
+      return right({
+        args,
+        sourceCode,
+        status: 'loaded',
+        warnings: [],
+      })
+    } catch (err: unknown) {
+      return left(`file ${args.input} could not be opened due to ${err}`)
+    }
+  } else {
+    return left(`file ${args.input} does not exist`)
+  }
+}
+
 
 /**
  * Parse a TNT specification.
  *
- * @param argv parameters as provided by yargs
+ * @param loaded the procedure status produced by `load`
  */
-function parse(argv: any): Either<String, [ParserPhase2, string]> {
-  return loadFile(argv.input).chain(text => parseText(argv, text))
-}
-
-/**
- * Check types and effects of a TNT specification.
- *
- * @param argv parameters as provided by yargs
- */
-// TODO Should return Phase2 parsing data + typing data
-function typecheck(argv: any): Either<String, void> {
-  return parse(argv).chain(d => {
-    const [parseData, sourceCode] = d
-    const finder = lineColumn(sourceCode)
-    const definitionsTable = parseData.table
-    const [typeErrors, types] = inferTypes(parseData.module, definitionsTable)
-    types.forEach((value, key) => console.log(`${key}: ${typeSchemeToString(value)}`))
-
-    typeErrors.forEach((value, key) => {
-      const loc = parseData.sourceMap.get(key)!
-      const message: ErrorMessage = {
-        explanation: errorTreeToString(value),
-        locs: [loc],
-      }
-
-      console.error(formatError(sourceCode, finder, message))
-    })
-
-    const [errors, effects] = inferEffects(definitionsTable, parseData.module)
-    effects.forEach((value, key) => console.log(`${key}: ${effectToString(value)}`))
-
-    errors.forEach((value, key) => {
-      const loc = parseData.sourceMap.get(key)!
-      const message: ErrorMessage = {
-        explanation: errorTreeToString(value),
-        locs: [loc],
-      }
-
-      console.error(formatError(sourceCode, finder, message))
-    })
-
-    return checkModes(parseData.module, effects)
-      .map(e => e.forEach((value, key) => console.log(`${key}: ${value}`)))
-      .mapLeft(e => {
-        e.forEach((value, key) => {
-          const loc = parseData.sourceMap.get(key)!
-          const message: ErrorMessage = {
-            explanation: errorTreeToString(value),
-            locs: [loc],
-          }
-          console.error(formatError(sourceCode, finder, message))
-        })
-
-        return "typechecking failed"
-      })
-  })
-}
-
-/**
- * Run REPL.
- *
- * @param _argv parameters as provided by yargs
- */
-function runRepl(_argv: any) {
-  tntRepl(process.stdin, process.stdout)
-}
-
-// Load a file into a string
-function loadFile(p: PathLike): Either<string, string> {
-  if (existsSync(p)) {
-    try {
-      return right(readFileSync(p, 'utf8'))
-    } catch (err: unknown) {
-      return left(`file ${p} could not be opened due to ${err}`)
-    }
-  } else {
-    return left(`file ${p} does not exist`)
-  }
-}
-
-// a callback to parse the text that we get from readFile
-function parseText(argv: any, text: string): Either<String, [ParserPhase2, string]> {
-  const path = resolve(cwd(), argv.input)
-  return parsePhase1(text, path)
+export function parse(loaded: LoadedStatus): Either<String, ParsedStatus> {
+  const { args, sourceCode } = loaded
+  const path = resolve(cwd(), args.input)
+  return parsePhase1(sourceCode, path)
     .mapLeft(errs => {
-      reportParseError(argv, text, errs)
+      reportParseError(args, sourceCode, errs)
       return "parsing failed in phase 1"
     })
     .chain(phase1Data => {
-      if (argv.sourceMap) {
+      if (args.sourceMap) {
         // Write source map to the specified file
-        writeToJson(argv.sourceMap, compactSourceMap(phase1Data.sourceMap))
+        writeToJson(args.sourceMap, compactSourceMap(phase1Data.sourceMap))
       }
       return parsePhase2(phase1Data)
         .mapLeft(errs => {
-          reportParseError(argv, text, errs)
+          reportParseError(args, sourceCode, errs)
           return "parsing failed in phase 2"
         })
     }).map(phase2Data => {
       // TODO move into helper combinator
-      if (argv.out) {
+      if (args.out) {
         // write the parsed IR to the output file
-        writeToJson(argv.out, {
+        writeToJson(args.out, {
           status: 'parsed',
           warnings: [],
           module: phase2Data.module,
         })
       }
-      return [phase2Data, text]
+      return { ...loaded, ...phase2Data, status: 'parsed' }
     })
 }
 
@@ -156,6 +133,78 @@ function reportParseError(argv: any, sourceCode: string, errors: ErrorMessage[])
     // write the errors to stderr
     errors.forEach((m) => console.error(formatError(sourceCode, finder, m)))
   }
+}
+
+
+/**
+ * Check types and effects of a TNT specification.
+ *
+ * @param parsed the procedure status produced by `parse`
+ */
+// TODO Should return Phase2 parsing data + typing data
+export function typecheck(parsed: ParsedStatus): Either<String, TypecheckedStatus> {
+  const { sourceCode, table, module, sourceMap } = parsed
+  const finder = lineColumn(sourceCode)
+  const definitionsTable = table
+  const [typeErrors, types] = inferTypes(module, definitionsTable)
+  types.forEach((value, key) => console.log(`${key}: ${typeSchemeToString(value)}`))
+
+  typeErrors.forEach((value, key) => {
+    const loc = sourceMap.get(key)!
+    const message: ErrorMessage = {
+      explanation: errorTreeToString(value),
+      locs: [loc],
+    }
+
+    console.error(formatError(sourceCode, finder, message))
+  })
+
+  const [errors, effects] = inferEffects(definitionsTable, module)
+  effects.forEach((value, key) => console.log(`${key}: ${effectToString(value)}`))
+
+  errors.forEach((value, key) => {
+    const loc = sourceMap.get(key)!
+    const message: ErrorMessage = {
+      explanation: errorTreeToString(value),
+      locs: [loc],
+    }
+
+    console.error(formatError(sourceCode, finder, message))
+  })
+
+  return checkModes(module, effects)
+    .map(e => e.forEach((value, key) => console.log(`${key}: ${value}`)))
+    .mapLeft(e => {
+      e.forEach((value, key) => {
+        const loc = sourceMap.get(key)!
+        const message: ErrorMessage = {
+          explanation: errorTreeToString(value),
+          locs: [loc],
+        }
+        console.error(formatError(sourceCode, finder, message))
+      })
+
+      return "typechecking failed"
+    })
+    .map(_ => { return { ...parsed, types, effects, status: 'typechecked' } })
+}
+
+/**
+ * Run REPL.
+ *
+ * @param _argv parameters as provided by yargs
+ */
+function runRepl(_argv: any) {
+  tntRepl(process.stdin, process.stdout)
+}
+
+/** Write the OutputStatus of the procedureStatus as JSON, if --out is set */
+function withJsonToOutput(procedureStatus: ProcedureStatus): Either<String, OutputStatus> {
+  // Separate the arg data from the output status data
+  const { args, ...outputData } = procedureStatus
+  // TODO test/handle case of writing to non-existent file
+  if (args.out) writeToJson(args.out, outputData);
+  return right(outputData)
 }
 
 /**
@@ -191,7 +240,12 @@ const parseCmd = {
         desc: 'name of the source map',
         type: 'string',
       }),
-  handler: handleCmd(parse),
+  handler: handleCmd(
+    args =>
+      load(args)
+        .chain(parse)
+        .chain(withJsonToOutput)
+  ),
 }
 
 // construct typecheck commands with yargs
@@ -204,7 +258,13 @@ const typecheckCmd = {
         desc: 'output file',
         type: 'string',
       }),
-  handler: handleCmd(typecheck),
+  handler: handleCmd(
+    args =>
+      load(args)
+        .chain(parse)
+        .chain(typecheck)
+        .chain(withJsonToOutput)
+  ),
 }
 
 // construct repl commands with yargs
