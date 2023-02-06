6acae7477b2119789c311b784da0cf5c1e9a23ed
Shon Feder <shon@informal.systems>
2023-02-05 21:39:32 -0500

    Remove errant commas from quint-mode
    Some commas found there way into the quoted list. Not sure when these
    slipped in, but no time recently!

--- editor-plugins/emacs/quint-mode.el
+++ editor-plugins/emacs/quint-mode.el
@@ -22,7 +22,7 @@
 
 ;; define several category of keywords
 (defconst quint-types '("int" "str" "bool" "Set" "List"))
-(defconst quint-keywords '("Rec", "Tup", "not" "and" "or" "iff" "implies" "all" "any" "if" "else"))
+(defconst quint-keywords '("Rec" "Tup" "not" "and" "or" "iff" "implies" "all" "any" "if" "else"))
 (defconst quint-declarations '("module" "import" "const" "var" "def" "val" "pure"
                              "nondet" "action" "temporal" "assume" "type"))
 (defconst quint-constants '("Bool" "Int" "Nat" "false" "true"))
