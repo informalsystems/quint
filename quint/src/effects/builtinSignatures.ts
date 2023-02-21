04fd8c46fdf8d4aa083aeaf94b6125701e0ba3b4
bugarela <gabrielamoreira05@gmail.com>
2023-02-21 09:30:12 -0300

    Generalize effect propagation and propagate temporal on all standard operators

--- quint/src/effects/builtinSignatures.ts
+++ quint/src/effects/builtinSignatures.ts
@@ -12,160 +12,225 @@
  * @module
  */
 
-import { EffectScheme, Signature, effectNames } from './base'
+import { ComponentKind, Effect, EffectComponent, EffectScheme, Signature, Variables, effectNames, toScheme } from './base'
 import { parseEffectOrThrow } from './parser'
-import { range, times, zip } from 'lodash'
+import { range, times } from 'lodash'
 
 export function getSignatures(): Map<string, Signature> {
   return new Map<string, Signature>(fixedAritySignatures.concat(multipleAritySignatures))
 }
-
-const literals = ['Nat', 'Int', 'Bool'].map(name => ({ name, effect: 'Pure' }))
 
 function parseAndQuantify(effectString: string): EffectScheme {
   const e = parseEffectOrThrow(effectString)
   return { effect: e, ...effectNames(e) }
 }
 
+const prefixByComponentKind = new Map<ComponentKind, string>([
+  ['read', 'r'],
+  ['temporal', 't'],
+  ['update', 'u'],
+])
+
+/**
+ * Builds a signature that propagates components of the given kinds
+ *
+ * Example: propagateComponents(['read', 'temporal'])(2) results in
+ * (Read[r1] & Temporal[t1], Read[r2] & Temporal[t2]) => Read[r1, r2] & Temporal[t1, t2]
+ *
+ * @param kinds the kinds of components to propagate
+ * @returns an arrow function taking arity and returning the effect for that arity
+ */
+function propagateComponents(kinds: ComponentKind[]): ((arity: number) => EffectScheme) {
+  return (arity: number) => {
+    const params: Effect[] = times(arity, i => {
+      const components: EffectComponent[] = kinds.map(kind => {
+        return { kind: kind, variables: { kind: 'quantified', name: `${prefixByComponentKind.get(kind)!}${i + 1}` } }
+      })
+
+      return { kind: 'concrete', components: components }
+    })
+
+    const resultComponents: EffectComponent[] = kinds.map(kind => {
+      const names = times(arity, i => `${prefixByComponentKind.get(kind)!}${i + 1}`)
+      const variables: Variables[] = names.map(name => ({ kind: 'quantified', name }))
+      return { kind: kind, variables: { kind: 'union', variables } }
+    })
+
+    const result: Effect = { kind: 'concrete', components: resultComponents }
+    const effect: Effect = { kind: 'arrow', params: params, result: result }
+
+    return { effect, ...effectNames(effect) }
+  }
+}
+
+const p = parseAndQuantify
+const standardPropagation = propagateComponents(['read', 'temporal'])
+
+/**
+ * Builds a signature that propagates components of the given kinds, including a lambda parameter
+ * for the last argument
+ *
+ * Used in operators like `map`, `filter` and `fold`
+ *
+ * Example: propagationWithLambda(['read', 'temporal'])(2) results in
+ * (Read[r1] & Temporal[t1], (Read[r1] & Temporal[t1]) => Read[r2] & Temporal[t2]) => Read[r1, r2] & Temporal[t1, t2]
+ *
+ * @param kinds the kinds of components to propagate
+ * @returns an arrow function taking arity and returning the effect for that arity
+ */
+function propagationWithLambda(kinds: ComponentKind[]): ((arity: number) => EffectScheme) {
+  return (arity: number) => {
+    const params: Effect[] = times(arity - 1, i => {
+      const components: EffectComponent[] = kinds.map(kind => {
+        return { kind: kind, variables: { kind: 'quantified', name: `${prefixByComponentKind.get(kind)!}${i + 1}` } }
+      })
+
+      return { kind: 'concrete', components: components }
+    })
+
+    const lambdaResult: Effect = {
+      kind: 'concrete', components: kinds.map(kind => {
+        return { kind: kind, variables: { kind: 'quantified', name: `${prefixByComponentKind.get(kind)!}${arity}` } }
+      }),
+    }
+
+    const lambda: Effect = { kind: 'arrow', params, result: lambdaResult }
+
+    const resultComponents: EffectComponent[] = kinds.map(kind => {
+      const names = times(arity, i => `${prefixByComponentKind.get(kind)!}${i + 1}`)
+      const variables: Variables[] = names.map(name => ({ kind: 'quantified', name }))
+      return { kind: kind, variables: { kind: 'union', variables } }
+    })
+
+    const result: Effect = { kind: 'concrete', components: resultComponents }
+
+    const effect: Effect = { kind: 'arrow', params: params.concat(lambda), result }
+    return { effect, ...effectNames(effect) }
+  }
+}
+
+
+const literals = ['Nat', 'Int', 'Bool'].map(name => ({ name, effect: toScheme({ kind: 'concrete', components: [] }) }))
 const booleanOperators = [
-  { name: 'eq', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'neq', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'not', effect: '(Read[r1] & Temporal[t1]) => Read[r1] & Temporal[t1]' },
-  { name: 'iff', effect: '(Read[r1] & Temporal[t1], Read[r2] & Temporal[t2]) => Read[r1, r2] & Temporal[t1, t2]' },
-  { name: 'implies', effect: '(Read[r1] & Temporal[t1], Read[r2] & Temporal[t2]) => Read[r1, r2] & Temporal[t1, t2]' },
+  { name: 'eq', effect: standardPropagation(2) },
+  { name: 'neq', effect: standardPropagation(2) },
+  { name: 'not', effect: standardPropagation(1) },
+  { name: 'iff', effect: standardPropagation(2) },
+  { name: 'implies', effect: standardPropagation(2) },
 ]
 
 const setOperators = [
-  { name: 'guess', effect: '(Read[r1], (Read[r1]) => Read[r2] & Update[u]) => Read[r1, r2] & Update[u]' },
-  { name: 'exists', effect: '(Read[r1] & Temporal[t1], (Read[r1] & Temporal[t1]) => Read[r2] & Temporal[t2]) => Read[r1, r2] & Temporal[t1, t2]' },
-  { name: 'forall', effect: '(Read[r1] & Temporal[t1], (Read[r1] & Temporal[t1]) => Read[r2] & Temporal[t2]) => Read[r1, r2] & Temporal[t1, t2]' },
-  { name: 'in', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'contains', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'notin', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'union', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'intersect', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'exclude', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'subseteq', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'filter', effect: '(Read[r1], (Read[r1]) => Read[r2]) => Read[r1, r2]' },
-  { name: 'map', effect: '(Read[r1], (Read[r1]) => Read[r2]) => Read[r1, r2]' },
-  { name: 'fold', effect: '(Read[r1], Read[r2], (Read[r2], Read[r1]) => Read[r3]) => Read[r1, r2, r3]' },
-  { name: 'powerset', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'flatten', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'allLists', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'chooseSome', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'oneOf', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'isFinite', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'size', effect: '(Read[r1]) => Read[r1]' },
+  { name: 'guess', effect: p('(Read[r1], (Read[r1]) => Read[r2] & Update[u]) => Read[r1, r2] & Update[u]') },
+  { name: 'exists', effect: propagationWithLambda(['read', 'temporal'])(2) },
+  { name: 'forall', effect: propagationWithLambda(['read', 'temporal'])(2) },
+  { name: 'in', effect: standardPropagation(2) },
+  { name: 'contains', effect: standardPropagation(2) },
+  { name: 'notin', effect: standardPropagation(2) },
+  { name: 'union', effect: standardPropagation(2) },
+  { name: 'intersect', effect: standardPropagation(2) },
+  { name: 'exclude', effect: standardPropagation(2) },
+  { name: 'subseteq', effect: standardPropagation(2) },
+  { name: 'filter', effect: propagationWithLambda(['read', 'temporal'])(2) },
+  { name: 'map', effect: propagationWithLambda(['read', 'temporal'])(2) },
+  { name: 'fold', effect: propagationWithLambda(['read', 'temporal'])(3) },
+  { name: 'powerset', effect: standardPropagation(1) },
+  { name: 'flatten', effect: standardPropagation(1) },
+  { name: 'allLists', effect: standardPropagation(1) },
+  { name: 'chooseSome', effect: standardPropagation(1) },
+  { name: 'oneOf', effect: standardPropagation(1) },
+  { name: 'isFinite', effect: standardPropagation(1) },
+  { name: 'size', effect: standardPropagation(1) },
 ]
 
 const mapOperators = [
-  { name: 'get', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'keys', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'mapBy', effect: '(Read[r1], (Read[r1]) => Read[r2]) => Read[r1, r2]' },
-  { name: 'setToMap', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'setOfMaps', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'set', effect: '(Read[r1], Read[r2], Read[r3]) => Read[r1, r2, r3]' },
-  { name: 'setBy', effect: '(Read[r1], Read[r2], (Read[r1]) => Read[r3]) => Read[r1, r2, r3]' },
-  { name: 'put', effect: '(Read[r1], Read[r2], Read[r3]) => Read[r1, r2, r3]' },
+  { name: 'get', effect: standardPropagation(2) },
+  { name: 'keys', effect: standardPropagation(1) },
+  { name: 'mapBy', effect: propagationWithLambda(['read', 'temporal'])(2) },
+  { name: 'setToMap', effect: standardPropagation(1) },
+  { name: 'setOfMaps', effect: standardPropagation(2) },
+  { name: 'set', effect: standardPropagation(3) },
+  { name: 'setBy', effect: p('(Read[r1], Read[r2], (Read[r1]) => Read[r3]) => Read[r1, r2, r3]') },
+  { name: 'put', effect: standardPropagation(3) },
 ]
 
 const recordOperators = [
   // Keys should be pure, as we don't allow dynamic key access.
-  { name: 'field', effect: '(Read[r], Pure) => Read[r]' },
-  { name: 'fieldNames', effect: '(Read[r]) => Read[r]' },
-  { name: 'with', effect: '(Read[r1], Pure, Read[r2]) => Read[r1, r2]' },
+  { name: 'field', effect: p('(Read[r], Pure) => Read[r]') },
+  { name: 'fieldNames', effect: p('(Read[r]) => Read[r]') },
+  { name: 'with', effect: p('(Read[r1], Pure, Read[r2]) => Read[r1, r2]') },
 ]
 
 const tupleOperators = [
   // Indexes for tuples should be pure, as we don't allow dynamic tuple access.
-  { name: 'item', effect: '(Read[r1], Pure) => Read[r1]' },
+  { name: 'item', effect: p('(Read[r1], Pure) => Read[r1]') },
 ]
 
 const listOperators = [
-  { name: 'append', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'concat', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'head', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'tail', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'length', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'nth', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'indices', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'replaceAt', effect: '(Read[r1], Read[r2], Read[r3]) => Read[r1, r2, r3]' },
-  { name: 'slice', effect: '(Read[r1], Read[r2], Read[r3]) => Read[r1, r2, r3]' },
-  { name: 'range', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'select', effect: '(Read[r1], (Read[r1]) => Read[r2]) => Read[r1, r2]' },
-  { name: 'foldl', effect: '(Read[r1], Read[r2], (Read[r2], Read[r1]) => Read[r3]) => Read[r1, r2, r3]' },
-  { name: 'foldr', effect: '(Read[r1], Read[r2], (Read[r1], Read[r2]) => Read[r3]) => Read[r1, r2, r3]' },
+  { name: 'append', effect: standardPropagation(2) },
+  { name: 'concat', effect: standardPropagation(2) },
+  { name: 'head', effect: standardPropagation(1) },
+  { name: 'tail', effect: standardPropagation(1) },
+  { name: 'length', effect: standardPropagation(1) },
+  { name: 'nth', effect: standardPropagation(2) },
+  { name: 'indices', effect: standardPropagation(1) },
+  { name: 'replaceAt', effect: standardPropagation(3) },
+  { name: 'slice', effect: standardPropagation(3) },
+  { name: 'range', effect: standardPropagation(2) },
+  { name: 'select', effect: propagationWithLambda(['read', 'temporal'])(2) },
+  { name: 'foldl', effect: propagationWithLambda(['read', 'temporal'])(3) },
+  { name: 'foldr', effect: propagationWithLambda(['read', 'temporal'])(3) },
 ]
 
 const integerOperators = [
-  { name: 'iadd', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'isub', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'iuminus', effect: '(Read[r1]) => Read[r1]' },
-  { name: 'imul', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'idiv', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'imod', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'ipow', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'ilt', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'igt', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'ilte', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'igte', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
-  { name: 'to', effect: '(Read[r1], Read[r2]) => Read[r1, r2]' },
+  { name: 'iadd', effect: standardPropagation(2) },
+  { name: 'isub', effect: standardPropagation(2) },
+  { name: 'iuminus', effect: standardPropagation(1) },
+  { name: 'imul', effect: standardPropagation(2) },
+  { name: 'idiv', effect: standardPropagation(2) },
+  { name: 'imod', effect: standardPropagation(2) },
+  { name: 'ipow', effect: standardPropagation(2) },
+  { name: 'ilt', effect: standardPropagation(2) },
+  { name: 'igt', effect: standardPropagation(2) },
+  { name: 'ilte', effect: standardPropagation(2) },
+  { name: 'igte', effect: standardPropagation(2) },
+  { name: 'to', effect: standardPropagation(2) },
 ]
 
 const temporalOperators = [
-  { name: 'always', effect: '(Read[r] & Temporal[t]) => Temporal[r, t]' },
-  { name: 'eventually', effect: '(Read[r] & Temporal[t]) => Temporal[r, t]' },
-  { name: 'next', effect: '(Read[r]) => Temporal[r]' },
-  { name: 'orKeep', effect: '(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]' },
-  { name: 'mustChange', effect: '(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]' },
+  { name: 'always', effect: p('(Read[r] & Temporal[t]) => Temporal[r, t]') },
+  { name: 'eventually', effect: p('(Read[r] & Temporal[t]) => Temporal[r, t]') },
+  { name: 'next', effect: p('(Read[r]) => Temporal[r]') },
+  { name: 'orKeep', effect: p('(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]') },
+  { name: 'mustChange', effect: p('(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]') },
   // Enabled: Should we do this? https://github.com/informalsystems/quint/discussions/109
   // Or should the result be temporal?
-  { name: 'enabled', effect: '(Read[r1] & Update[u1]) => Read[r1]' },
-  { name: 'weakFair', effect: '(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]' },
-  { name: 'strongFair', effect: '(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]' },
+  { name: 'enabled', effect: p('(Read[r1] & Update[u1]) => Read[r1]') },
+  { name: 'weakFair', effect: p('(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]') },
+  { name: 'strongFair', effect: p('(Read[r] & Update[u], Read[v]) => Temporal[r, u, v]') },
 ]
 
 const otherOperators = [
-  { name: 'assign', effect: '(Read[r1], Read[r2]) => Read[r2] & Update[r1]' },
-  { name: 'then', effect: '(Read[r1] & Update[u], Read[r2] & Update[u]) => Read[r] & Update[u]' },
-  {
-    name: 'repeated',
-    effect: '(Read[r] & Update[u], Pure) => Read[r] & Update[u]',
-  },
-  {
-    name: 'fail',
-    effect: '(Read[r] & Update[u]) => Read[r] & Update[u]',
-  },
-  { name: 'assert', effect: '(Read[r]) => Read[r]' },
-  { name: 'ite', effect: '(Read[r1], Read[r2] & Update[u], Read[r3] & Update[u]) => Read[r1, r2, r3] & Update[u]' },
+  { name: 'assign', effect: p('(Read[r1], Read[r2]) => Read[r2] & Update[r1]') },
+  { name: 'then', effect: p('(Read[r1] & Update[u], Read[r2] & Update[u]) => Read[r] & Update[u]') },
+  { name: 'repeated', effect: p('(Read[r] & Update[u], Pure) => Read[r] & Update[u]') },
+  { name: 'fail', effect: propagateComponents(['read', 'update'])(1) },
+  { name: 'assert', effect: propagateComponents(['read'])(1) },
+  { name: 'ite', effect: p('(Read[r1], Read[r2] & Update[u], Read[r3] & Update[u]) => Read[r1, r2, r3] & Update[u]') },
 ]
 
-const readManyEffect = (arity: number) => {
-  const readVars = times(arity, i => `r${i}`)
-  const args = readVars.map(r => `Read[${r}]`)
-  return parseAndQuantify(`(${args.join(', ')}) => Read[${readVars.join(', ')}]`)
-}
-
-const readAndTemporalManyEffect = (arity: number) => {
-  const readVars = times(arity, i => `r${i}`)
-  const temporalVars = times(arity, i => `t${i}`)
-  const args = zip(readVars, temporalVars).map(([r, t]) => `Read[${r}] & Temporal[${t}]`)
-  return parseAndQuantify(`(${args.join(', ')}) => Read[${readVars.join(', ')}] & Temporal[${temporalVars.join(', ')}]`)
-}
-
 const multipleAritySignatures: [string, Signature][] = [
-  ['List', readManyEffect],
-  ['Set', readManyEffect],
-  ['Map', readManyEffect],
-  ['Rec', readManyEffect],
-  ['Tup', readManyEffect],
-  ['tuples', readManyEffect],
-  ['and', readAndTemporalManyEffect],
-  ['or', readAndTemporalManyEffect],
+  ['List', standardPropagation],
+  ['Set', standardPropagation],
+  ['Map', standardPropagation],
+  ['Rec', standardPropagation],
+  ['Tup', standardPropagation],
+  ['tuples', standardPropagation],
+  ['and', standardPropagation],
+  ['or', standardPropagation],
   ['unionMatch', (arity: number) => {
     const readVars = times((arity - 1) / 2, i => `r${i}`)
     const args = readVars.map(r => `Pure, (Pure) => Read[${r}]`)
-    return parseAndQuantify(`(Read[r], ${args.join(', ')}) => Read[${readVars.join(', ')}]`)
+    return p(`(Read[r], ${args.join(', ')}) => Read[${readVars.join(', ')}]`)
   }],
   ['actionAll', (arity: number) => {
     const indexes = range(arity)
@@ -173,14 +238,15 @@ const multipleAritySignatures: [string, Signature][] = [
     const args = indexes.map(i => `Read[r${i}] & Update[u${i}]`)
     const readVars = indexes.map(i => `r${i}`).join(', ')
     const updateVars = indexes.map(i => `u${i}`).join(', ')
-    return parseAndQuantify(`(${args.join(', ')}) => Read[${readVars}] & Update[${updateVars}]`)
+    return p(`(${args.join(', ')}) => Read[${readVars}] & Update[${updateVars}]`)
   }],
+  ['actionAll', propagateComponents(['read', 'update'])],
   ['actionAny', (arity: number) => {
     const indexes = range(arity)
 
     const args = indexes.map(i => `Read[r${i}] & Update[u]`)
     const readVars = indexes.map(i => `r${i}`).join(', ')
-    return parseAndQuantify(`(${args.join(', ')}) => Read[${readVars}] & Update[u]`)
+    return p(`(${args.join(', ')}) => Read[${readVars}] & Update[u]`)
   }],
 ]
 
@@ -196,5 +262,5 @@ const fixedAritySignatures: [string, Signature][] = [
   temporalOperators,
   otherOperators,
 ].flat().map(sig => [sig.name, ((_: number) => {
-  return parseAndQuantify(sig.effect)
+  return sig.effect
 })])
