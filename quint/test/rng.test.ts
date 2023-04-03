9f2be51e6f2f24512699c1611928970d7a90f311
Igor Konnov <igor@informal.systems>
2023-04-03 18:49:42 +0200

    break down the rng tests

--- quint/test/rng.test.ts
+++ quint/test/rng.test.ts
@@ -13,18 +13,43 @@ function testForRange(rng: Rng, bound: bigint, nsamples: number) {
 }
 
 describe('rng', () => {
-  it('produces big integers in the expected range', () => {
-    // initialize with a random seed
-    const rng = newRng()
-    // produce some numbers in various ranges
-    testForRange(rng, 10000n, 100000)
-    testForRange(rng, 2n ** 16n, 10000)
-    testForRange(rng, 2n ** 32n, 10000)
-    testForRange(rng, 2n ** 32n + 1n, 10000)
-    testForRange(rng, 2n ** 64n + 1n, 10000)
-    testForRange(rng, 2n ** 64n + 3n * 5n * 7n * 11n, 10000)
-    testForRange(rng, 2n ** 96n + 1n, 10000)
-    testForRange(rng, 2n ** 96n + 3n * 5n * 7n * 11n, 10000)
-    testForRange(rng, 2n ** 256n + 2n ** 33n + 11n, 10000)
+  it('produces big integers in [0, 10000)', () => {
+    testForRange(newRng(), 10000n, 100000)
+  })
+
+  it('produces big integers in [0, 2^16)', () => {
+    testForRange(newRng(), 2n ** 16n, 10000)
+  })
+
+  it('produces big integers in [0, 2^32)', () => {
+    testForRange(newRng(), 2n ** 32n, 10000)
+  })
+
+  it('produces big integers in [0, 2^32 + 1))', () => {
+    testForRange(newRng(), 2n ** 32n + 1n, 10000)
+  })
+
+  it('produces big integers in [0, 2^64))', () => {
+    testForRange(newRng(), 2n ** 64n, 10000)
+  })
+
+  it('produces big integers in [0, 2^64 + 1))', () => {
+    testForRange(newRng(), 2n ** 64n + 1n, 10000)
+  })
+
+  it('produces big integers in [0, over 2^64))', () => {
+    testForRange(newRng(), 2n ** 64n + 3n * 5n * 7n * 11n, 10000)
+  })
+
+  it('produces big integers in [0, 2^96 + 1))', () => {
+    testForRange(newRng(), 2n ** 96n + 1n, 10000)
+  })
+
+  it('produces big integers in [0, over 2^96))', () => {
+    testForRange(newRng(), 2n ** 96n + 3n * 5n * 7n * 11n, 10000)
+  })
+
+  it('produces big integers in [0, over 2^256))', () => {
+    testForRange(newRng(), 2n ** 256n + 2n ** 33n + 11n, 10000)
   })
 })
