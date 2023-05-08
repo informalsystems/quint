d79213e8480165a1ae2cec07a30de2642063fc6c
Shon Feder <shon@informal.systems>
2023-05-07 21:28:10 -0400

    Add test for corrupted jar file

--- quint/src/quintVerifier.ts
+++ quint/src/quintVerifier.ts
@@ -14,11 +14,11 @@
 
 import { Either, left, right } from '@sweet-monads/either'
 import { ErrorMessage } from './quintParserFrontend'
-import { execSync } from 'child_process'
-import * as path from 'path'
-import * as fs from 'fs'
-import * as grpc from '@grpc/grpc-js'
-import * as proto from '@grpc/proto-loader'
+import { execSync, spawnSync } from 'child_process'
+import path from 'path'
+import fs from 'fs'
+import grpc from '@grpc/grpc-js'
+import proto from '@grpc/proto-loader'
 
 // TODO const APALACHE_VERSION = "0.30.8"
 // TODO const DEFAULT_HOME = path.join(__dirname, 'apalache')
@@ -73,36 +73,50 @@ function findDist(): VerifyResult<ApalacheDist> {
   })
 }
 
-function grpcClient(dist: ApalacheDist): VerifyResult<ApalacheGrpc> {
-  const tmpDir = fs.mkdtempSync('apalache-proto-')
+const confirmJarUtilInstalled = (): VerifyResult<null> =>
+  (spawnSync('jar', ['--version']).status === 0)
+    ? right(null)
+    : err('The `jar` utility must be installed')
+
+const unpackProtoFile = (jar: string): () => VerifyResult<string> => {
   const protoFileName = 'cmdExecutor.proto'
-  // TODO: error handling
-  execSync(`jar xf ${dist.jar} ${protoFileName}`, { cwd: tmpDir })
-  const protoFile = path.join(tmpDir, protoFileName)
-
-  // See https://grpc.io/docs/languages/node/basics/#example-code-and-setup
-  const options = {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true,
-  }
-
-  const pkg = proto.loadSync(protoFile, options)
-  const protoDescriptor = grpc.loadPackageDefinition(pkg)
-  // The protoDescriptor object has the full package hierarchy
-  const routeguide = protoDescriptor.routeguide as any // gRPCs typing gets intractable here
-  return right(new routeguide.RouteGuide(APALACHE_SERVER_URI, grpc.credentials.createInsecure()))
+  const tmpDir = fs.mkdtempSync('apalache-proto-')
+  return () =>
+    (spawnSync('jar', ['xf', jar, protoFileName], { cwd: tmpDir }).status === 0)
+      ? right(path.join(tmpDir, protoFileName))
+      : err('Apalache distribution is corrupted. Could not extract proto file from apalache.jar')
 }
 
+
+// See https://grpc.io/docs/languages/node/basics/#example-code-and-setup
+const grpcStubOptions = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+}
+
+const grpcClient = (dist: ApalacheDist): VerifyResult<ApalacheGrpc> =>
+  confirmJarUtilInstalled()
+    .chain(unpackProtoFile(dist.jar))
+    .chain(protoFile => {
+      const pkg = proto.loadSync(protoFile, grpcStubOptions)
+      const protoDescriptor = grpc.loadPackageDefinition(pkg)
+      // The protoDescriptor object has the full package hierarchy
+      const routeguide = protoDescriptor.routeguide as any // gRPCs typing gets intractable here
+      return right(new routeguide.RouteGuide(APALACHE_SERVER_URI, grpc.credentials.createInsecure()))
+    })
+
 function connect(stubAndDist: [ApalacheDist, ApalacheGrpc]): VerifyResult<Apalache> {
-  const [_TODO, stub] = stubAndDist
+  const [_TODO, _stub] = stubAndDist
   // TODO confirm server is running or else start it
-  return right({
-    check: (c: apalache_config) => stub.run({ cmd: 'CHECK', config: JSON.stringify(c) }, () => null),
-    stop: () => right(null), // TODO
-  })
+  // if started, ensure to kill it when done
+  // return right({
+  //   check: (c: apalache_config) => stub.run({ cmd: 'CHECK', config: JSON.stringify(c) }, () => null),
+  //   stop: () => right(null), // TODO
+  // })
+  return err('Apalache server')
 }
 
 
