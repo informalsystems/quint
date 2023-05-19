9f58bccb597d1dfb1f4c94cc17f6d3957c39a603
bugarela <gabrielamoreira05@gmail.com>
2023-05-19 16:53:38 -0300

    Add definition finder to `IRFinder`

--- quint/src/IRFinder.ts
+++ quint/src/IRFinder.ts
@@ -5,7 +5,7 @@
  * --------------------------------------------------------------------------------- */
 
 /**
- * Finding expressions in a module
+ * Finding expressions in a list of modules
  *
  * @author Gabriela Moreira
  *
@@ -13,35 +13,49 @@
  */
 
 import { IRVisitor, walkModule } from './IRVisitor'
-import { QuintEx, QuintModule } from './quintIr'
+import { QuintDef, QuintEx, QuintModule } from './quintIr'
 import { QuintType } from './quintTypes'
 
 /**
- * Finds a quint expression with a given id in a module
+ * Finds a quint expression with a given id in a list of modules
  *
- * @param module the module in which to search for the expression
+ * @param modules the modules in which to search for the expression
  * @param id the id to be searched for
  *
  * @returns a quint expression with the given id, or undefined if no expression is found
  */
-export function findExpressionWithId(module: QuintModule, id: bigint): QuintEx | undefined {
+export function findExpressionWithId(modules: QuintModule[], id: bigint): QuintEx | undefined {
   const visitor = new IRExpressionFinder(id)
-  walkModule(visitor, module)
+  modules.forEach(module => walkModule(visitor, module))
   return visitor.expression
 }
 
 /**
- * Finds a quint type with a given id in a module
+ * Finds a quint type with a given id in a list of modules
  *
- * @param module the module in which to search for the type
+ * @param modules the modules in which to search for the type
  * @param id the id to be searched for
  *
  * @returns a quint type with the given id, or undefined if no type is found
  */
-export function findTypeWithId(module: QuintModule, id: bigint): QuintType | undefined {
+export function findTypeWithId(modules: QuintModule[], id: bigint): QuintType | undefined {
   const visitor = new IRTypeFinder(id)
-  walkModule(visitor, module)
+  modules.forEach(module => walkModule(visitor, module))
   return visitor.type
+}
+
+/**
+ * Finds a quint definition with a given id in a list of modules
+ *
+ * @param modules the modules in which to search for the definition
+ * @param id the id to be searched for
+ *
+ * @returns a quint definition with the given id, or undefined if no expression is found
+ */
+export function findDefinitionWithId(modules: QuintModule[], id: bigint): QuintDef | undefined {
+  const visitor = new IRDefinitionFinder(id)
+  modules.forEach(module => walkModule(visitor, module))
+  return visitor.def
 }
 
 class IRExpressionFinder implements IRVisitor {
@@ -70,6 +84,21 @@ class IRTypeFinder implements IRVisitor {
   exitType(type: QuintType) {
     if (type.id === this.id) {
       this.type = type
+    }
+  }
+}
+
+class IRDefinitionFinder implements IRVisitor {
+  id: bigint
+  def?: QuintDef
+
+  constructor(id: bigint) {
+    this.id = id
+  }
+
+  exitDef(def: QuintDef) {
+    if (def.id === this.id) {
+      this.def = def
     }
   }
 }
--- quint/test/IRFinder.test.ts
+++ quint/test/IRFinder.test.ts
@@ -1,38 +1,55 @@
 import { assert } from 'chai'
 import { describe, it } from 'mocha'
-import { findExpressionWithId, findTypeWithId } from '../src/IRFinder'
+import { findDefinitionWithId, findExpressionWithId, findTypeWithId } from '../src/IRFinder'
 import { buildModuleWithDefs, buildModuleWithExpressions } from './builders/ir'
 
 describe('findExpressionWithId', () => {
-  const module = buildModuleWithExpressions(['Nat'])
+  const modules = [buildModuleWithExpressions(['Nat'])]
 
   it('finds expression for existing id', () => {
-    const expr = findExpressionWithId(module, 1n)
+    const expr = findExpressionWithId(modules, 1n)
 
     assert.isDefined(expr)
     assert.deepEqual(expr, { id: 1n, kind: 'name', name: 'Nat' })
   })
 
   it('returns undefined for inexisting id', () => {
-    const expr = findExpressionWithId(module, 99n)
+    const expr = findExpressionWithId(modules, 99n)
 
     assert.isUndefined(expr)
   })
 })
 
 describe('findTypeWithId', () => {
-  const module = buildModuleWithDefs(['const N: MY_TYPE'])
+  const modules = [buildModuleWithDefs(['const N: MY_TYPE'])]
 
   it('finds type for existing id', () => {
-    const type = findTypeWithId(module, 1n)
+    const type = findTypeWithId(modules, 1n)
 
     assert.isDefined(type)
     assert.deepEqual(type, { id: 1n, kind: 'const', name: 'MY_TYPE' })
   })
 
   it('returns undefined for inexisting id', () => {
-    const type = findTypeWithId(module, 99n)
+    const type = findTypeWithId(modules, 99n)
 
     assert.isUndefined(type)
+  })
+})
+
+describe('findDefinitionWithId', () => {
+  const modules = [buildModuleWithDefs(['val a = 1'])]
+
+  it('finds definition for existing id', () => {
+    const def = findDefinitionWithId(modules, 2n)
+
+    assert.isDefined(def)
+    assert.deepEqual(def, { id: 2n, kind: 'def', qualifier: 'val', name: 'a', expr: { id: 1n, kind: 'int', value: 1n }, doc: undefined })
+  })
+
+  it('returns undefined for inexisting id', () => {
+    const def = findDefinitionWithId(modules, 99n)
+
+    assert.isUndefined(def)
   })
 })
