129e95fc50742bba01ef302f8020d8ff6e734355
bugarela <gabrielamoreira05@gmail.com>
2023-06-15 16:42:45 -0300

    Add tests to new flattening function

--- quint/test/flatenning.test.ts
+++ quint/test/flatenning.test.ts
@@ -1,67 +1,39 @@
 import { assert } from 'chai'
 import { describe, it } from 'mocha'
-import { collectDefinitions } from '../src/definitionsCollector'
-import { scanConflicts } from '../src/definitionsScanner'
-import { flattenModules } from '../src/flattening'
+import { addDefToFlatModule, flattenModules } from '../src/flattening'
 import { newIdGenerator } from '../src/idGenerator'
-import { resolveImports } from '../src/importResolver'
 import { definitionToString } from '../src/IRprinting'
-import { resolveNames } from '../src/nameResolver'
-import { quintErrorToString } from '../src/quintError'
-import { treeFromModule } from '../src/scoping'
 import { collectIds } from './util'
-import JSONbig from 'json-bigint'
-import { parsePhase1fromText } from '../src/quintParserFrontend'
+import { parse } from '../src/quintParserFrontend'
 import { toScheme } from '../src/types/base'
+import { FlatModule } from '../src'
+import { SourceLookupPath } from '../src/sourceResolver'
 
-describe('flatten', () => {
+describe('flattenModules', () => {
   function assertFlatennedDefs(baseDefs: string[], defs: string[], expectedDefs: string[]): void {
     const idGenerator = newIdGenerator()
+    const fake_path: SourceLookupPath = { normalizedPath: 'fake_path', toSourceName: () => 'fake_path' }
 
     const quintModules: string = `module A { ${baseDefs.join('\n')} } module wrapper { ${defs.join('\n')} }`
 
-    const result = parsePhase1fromText(idGenerator, quintModules, 'mocked_path')
-    if (result.isLeft()) {
-      assert.fail(`Couldn't parse mocked expression. Result - ${JSONbig.stringify(result)}`)
+    const parseResult = parse(idGenerator, 'fake_location', fake_path, quintModules)
+    if (parseResult.isLeft()) {
+      assert.fail('Failed to parse mocked up module')
     }
+    const { modules, table, sourceMap } = parseResult.unwrap()
+    const [moduleA, _module] = modules
 
-    // Module A is a module called A containing `baseDefs`. The main module can import or instance it.
-    const moduleA = result.value.modules[0]
-    const tableA = collectDefinitions(moduleA)
-    const lookupTableA = resolveNames(moduleA, tableA, treeFromModule(moduleA)).unwrap()
-
-    // This is the main module containing `defs`. It can import or instance module A.
-    const module = result.value.modules[1]
-    const table = collectDefinitions(module)
-    const [errors, tableWithImports] = resolveImports(
-      module,
-      new Map([
-        ['A', tableA],
-        ['wrapper', table],
-      ])
-    )
-
-    assert.isEmpty(errors, `Should find no errors, found: ${[...errors.values()].map(quintErrorToString)}`)
-
-    const lookupTable = new Map([
-      ...lookupTableA.entries(),
-      ...resolveNames(module, tableWithImports, treeFromModule(module)).unwrap().entries(),
-    ])
-
-    const originalIds = [...result.value.sourceMap.keys()]
+    const originalIds = [...sourceMap.keys()]
     const typesMap = new Map(originalIds.map(i => [i, toScheme({ kind: 'int' })]))
 
-    const { flattenedModules, flattenedAnalysis } = flattenModules(
-      [moduleA, module],
-      lookupTable,
-      idGenerator,
-      result.value.sourceMap,
-      { types: typesMap, effects: new Map(), modes: new Map() }
-    )
-
+    const { flattenedModules, flattenedAnalysis } = flattenModules(modules, table, idGenerator, sourceMap, {
+      types: typesMap,
+      effects: new Map(),
+      modes: new Map(),
+    })
     const [_, flattenedModule] = flattenedModules
 
-    it('flattens instances', () => {
+    it('has all expected defs', () => {
       assert.sameDeepMembers(
         flattenedModule.defs.map(def => definitionToString(def)),
         expectedDefs
@@ -74,12 +46,7 @@ describe('flatten', () => {
       assert.sameDeepMembers(ids, [...new Set(ids)])
     })
 
-    it('does not have conflicting definitions', () => {
-      assert.isTrue(scanConflicts(table, treeFromModule(module)).isRight())
-    })
-
     it('adds new entries to the source map', () => {
-      const sourceMap = result.value.sourceMap
       assert.includeDeepMembers(
         [...sourceMap.keys()],
         flattenedModule.defs.map(def => def.id)
@@ -198,5 +165,99 @@ describe('flatten', () => {
     ]
 
     assertFlatennedDefs(baseDefs, defs, expectedDefs)
+  })
+})
+
+describe('addDefToFlatModule', () => {
+  function assertAddedDefs(baseDefs: string[], defToAdd: string, expectedDefs: string[]): void {
+    const idGenerator = newIdGenerator()
+    const fake_path: SourceLookupPath = { normalizedPath: 'fake_path', toSourceName: () => 'fake_path' }
+
+    const quintModules: string = `module A { ${baseDefs.join('\n')} } module wrapper { ${defToAdd} }`
+
+    const parseResult = parse(idGenerator, 'fake_location', fake_path, quintModules)
+    if (parseResult.isLeft()) {
+      assert.fail('Failed to parse mocked up module')
+    }
+    const { modules, table, sourceMap } = parseResult.unwrap()
+    const [moduleA, module] = modules
+
+    const originalIds = [...sourceMap.keys()]
+    const typesMap = new Map(originalIds.map(i => [i, toScheme({ kind: 'int' })]))
+
+    const def = module.defs[0]
+    const moduleWithoutDef: FlatModule = { ...module, defs: [] }
+    const { flattenedModule, flattenedAnalysis } = addDefToFlatModule(
+      modules,
+      table,
+      idGenerator,
+      sourceMap,
+      { types: typesMap, effects: new Map(), modes: new Map() },
+      moduleWithoutDef,
+      def
+    )
+
+    it('has all expected defs', () => {
+      assert.sameDeepMembers(
+        flattenedModule.defs.map(def => definitionToString(def)),
+        expectedDefs
+      )
+    })
+
+    it('does not repeat ids', () => {
+      const ids = collectIds(flattenedModule)
+      assert.notDeepInclude(collectIds(moduleA), ids)
+      assert.sameDeepMembers(ids, [...new Set(ids)])
+    })
+
+    it('adds new entries to the source map', () => {
+      assert.includeDeepMembers(
+        [...sourceMap.keys()],
+        flattenedModule.defs.map(def => def.id)
+      )
+    })
+
+    it('adds new entries to the types map', () => {
+      assert.includeDeepMembers(
+        [...flattenedAnalysis.types.keys()],
+        flattenedModule.defs.map(def => def.id)
+      )
+    })
+  }
+
+  describe('single instance with several definitions', () => {
+    const baseDefs = [
+      'const N: int',
+      'var x: int',
+      'pure def f(a) = a + 1',
+      `action V = x' = f(x)`,
+      'assume T = N > 0',
+      'def lam = val b = 1 { a => b + a }',
+      'def lam2 = val b = 1 { a => b + a }',
+    ]
+
+    const defToAdd = 'import A(N = 1) as A1'
+
+    const expectedDefs = [
+      'pure val A1::N: int = 1',
+      'var A1::x: int',
+      'pure def A1::f = ((A1::a) => iadd(A1::a, 1))',
+      `action A1::V = assign(A1::x, A1::f(A1::x))`,
+      'assume A1::T = igt(A1::N, 0)',
+      'def A1::lam = val A1::b = 1 { ((A1::a) => iadd(A1::b, A1::a)) }',
+      'def A1::lam2 = val A1::b = 1 { ((A1::a) => iadd(A1::b, A1::a)) }',
+    ]
+
+    assertAddedDefs(baseDefs, defToAdd, expectedDefs)
+  })
+
+  describe('imports', () => {
+    const baseDefs = ['val f(x) = x + 1']
+
+    const defToAdd = 'import A.*'
+
+    const expectedDefs = ['val f = ((x) => iadd(x, 1))']
+
+    assertAddedDefs(baseDefs, defToAdd, expectedDefs)
   })
 })
