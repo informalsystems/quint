a344a9a200f0aa15af1c1cb509b8409e12130220
Thomas Pani <thomas.pani@gmail.com>
2023-07-04 16:55:54 +0200

    Set `verify` args in example runner

--- examples/.scripts/run-example.sh
+++ examples/.scripts/run-example.sh
@@ -27,11 +27,21 @@ get_main () {
   echo "${main}"
 }
 
+get_verify_args () {
+  local file="$1"
+  local args=""
+  if [[ "$file" == "classic/distributed/LamportMutex/LamportMutex.qnt" ]] ; then
+    args="--init=Init --step=Next"
+  fi
+  echo "${args}"
+}
+
 file="$1"
 syntax="$(result "quint parse ${file}")"
 types="$(result "quint typecheck ${file}")"
 main="$(get_main "${file}")"
 tests="$(result "quint test ${main} ${file}")"
-verify="$(result "quint verify --max-steps=5 ${main} ${file}")"
+verify_args="$(get_verify_args "${file}")"
+verify="$(result "quint verify --max-steps=5 ${verify_args} ${main} ${file}")"
 
 echo "| [${file}](./${file}) | ${syntax} | ${types} | ${tests} | ${verify} |"
