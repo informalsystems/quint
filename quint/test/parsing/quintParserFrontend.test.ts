df0ca255517add1297a8c760ad77ebf4aab622ab
bugarela <gabrielamoreira05@gmail.com>
2023-10-16 16:02:57 -0300

    Remove extra blank line

--- quint/test/parsing/quintParserFrontend.test.ts
+++ quint/test/parsing/quintParserFrontend.test.ts
@@ -49,7 +49,6 @@ function parseAndCompare(artifact: string): void {
   const phase1Result = parsePhase1fromText(gen, readQuint(artifact), mainPath.toSourceName())
   const phase2Result = parsePhase2sourceResolution(gen, resolver, mainPath, phase1Result)
 
-
   const phase3Result = parsePhase3importAndNameResolution(phase2Result)
   const { modules, sourceMap, errors } = parsePhase4toposort(phase3Result)
 
