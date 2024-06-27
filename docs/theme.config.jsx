6e6ee31c47023c87bfc5afdae68e2f57577ab4f2
bugarela <gabrielamoreira05@gmail.com>
2024-06-27 14:35:05 -0300

    Add Telegram chat and override some Nextra defaults

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,5 +1,5 @@
-import { useTheme } from 'nextra-theme-docs'
-import { useConfig } from 'nextra-theme-docs'
+import { useTheme, useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router';
 
 export default {
   logo: () => {
@@ -14,8 +14,20 @@ export default {
       );
     }
   },
+  docsRepositoryBase: 'https://github.com/informalsystems/quint',
   project: {
     link: 'https://github.com/informalsystems/quint'
+  },
+  chat: {
+    link: 'https://t.me/quint_lang',
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 50 50">
+        <path
+          fill="currentColor"
+          d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z">
+        </path>
+      </svg>
+    )
   },
   head: function useHead() {
     const { title } = useConfig()
@@ -53,9 +65,24 @@ export default {
       </>
     )
   },
+  footer: {
+    text: (
+      <div className="flex w-full flex-col items-center sm:items-start">
+        <p className="mt-6 text-xs">
+          © {new Date().getFullYear()} Informal Systems.
+        </p>
+      </div>
+    )
+  },
   primaryHue: { dark: 264, light: 264 },
   primarySaturation: { dark: 52, light: 90 },
   sidebar: {
     defaultMenuCollapseLevel: 1
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    return {
+      titleTemplate: asPath !== '/' ? '%s – Quint' : 'Quint',
+    };
   }
 }
