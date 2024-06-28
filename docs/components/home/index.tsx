f61ee7584b6a70aec1e562aa9b021dba52b871a8
bugarela <gabrielamoreira05@gmail.com>
2024-06-28 09:28:27 -0300

    Change text on landing page

--- docs/components/home/index.tsx
+++ docs/components/home/index.tsx
@@ -25,9 +25,48 @@ export function Home() {
 
       <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <h1 className="text-4xl font-bold leading-tight text-inherit sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight font-pj">Quint: A modern and executable specification language</h1>
-        <div className="grid grid-cols-1 gap-y-4 lg:items-center lg:grid-cols-2 xl:grid-cols-2">
-          <div className="text-center xl:col-span-1 lg:text-left md:px-16 lg:px-0 xl:pr-20 my-8">
-            <dl>
+        <div className="grid grid-cols-1 gap-y-4 lg:items-center lg:grid-cols-3 xl:grid-cols-3">
+          <div className="text-center xl:col-span-2 lg:text-left md:px-16 lg:px-0 xl:pr-20 my-8">
+            <h3 className="text-2xl font-bold leading-relaxed">Executable</h3>
+            <p className="text-lg">In contrast to specifications written in <b>English and Markdown</b>, Quint specs are executable: you can run them, test them and check things about them.</p>
+            <ul className="list-inside list-[square] indent-4">
+              <li>English and Markdown: not executable</li>
+              <li>Quint: executable</li>
+            </ul>
+            <br />
+
+            <h3 className="text-2xl font-bold leading-relaxed">Specification language</h3>
+            <p className="text-lg">In contrast to a <b>programming language</b>, a specification language is made for defining things more abstract than programs, such as protocols.</p>
+            <ul className="list-inside list-[square] indent-4">
+              <li>Programming language: define how things happen in detail</li>
+              <li>Specification language: define only what you care about</li>
+            </ul>
+            <br />
+
+            <h3 className="text-2xl font-bold leading-relaxed">Modern</h3>
+            <p className="text-lg">In contrast to many <b>existing specification languages</b>, Quint tooling is similar to that of modern programming languages, and it’s syntax resembles them.</p>
+            <ul className="list-inside list-[square] indent-4">
+              <li>Existing languages: Mathy syntax, old GUI tools</li>
+              <li>Quint: Familiar syntax, CLI-first, tools for VSCode, Vim and Emacs</li>
+            </ul>
+
+            {/* <dl>
+              <dt className="text-4xl font-bold">Executable</dt>
+              <dd>In contrast to specifications written in English and Markdown, Quint specs are executable: you can run them, test them and check things about them
+                <ul>
+                  <li>English and Markdown: not executable</li>
+                  <li>Quint: executable</li>
+                </ul>
+              </dd>
+
+              <dt className="text-4xl font-bold">Specification Language</dt>
+              <dd></dd>
+
+              <dt className="text-4xl font-bold">Modern</dt>
+              <dd></dd>
+            </dl> */}
+
+            {/* <dl>
               <dt>A simple and familiar <strong>syntax</strong></dt>
               <dd>to support engineers reading and writing specifications</dd>
 
@@ -48,7 +87,7 @@ export function Home() {
 
               <dt>A symbolic <strong>model checker</strong></dt>
               <dd>to verify your specifications via <a href="https://github.com/informalsystems/apalache">Apalache</a></dd>
-            </dl>
+            </dl> */}
             {/* <p className="mt-2 text-lg text-gray-600 sm:mt-6 font-inter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vehicula massa in enim luctus. Rutrum arcu.</p> */}
 
             <a href="/docs/getting-started" title="" className="inline-flex px-8 py-4 mt-8 text-lg font-bold transition-all duration-200 bg-quint-purple text-white border border-transparent rounded sm:mt-10 font-pj hover:bg-[#2d0075] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900" role="button">
@@ -74,8 +113,8 @@ export function Home() {
             <img className="w-full mx-auto" src="https://cdn.rareblocks.xyz/collection/clarity/images/hero/1/illustration.png" alt="" />
           </div>
         </div>
-      </div>
-    </section>
+      </div >
+    </section >
   </div >
 
   )
