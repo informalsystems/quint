589cdec14fa2459bb55d91556af492a61b0b0b11
Aaron Wright <aaronmw@gmail.com>
2024-12-15 16:33:56 -0500

    Add newsletter signp form

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,22 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      primary: {
-        600: '#9d6ce5',
-      }
-    },
     extend: {
       colors: {
         'quint-purple': '#9d6ce5',
         'quint-orange': '#ffab70',
       },
-    }
+    },
   },
   plugins: [],
-};
+}
