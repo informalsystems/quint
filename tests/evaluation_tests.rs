8db25307b9f73e739ff56b1ece220967db28870b
bugarela <gabrielamoreira05@gmail.com>
2025-01-28 14:46:49 -0300

    Add simple evaluation test

---
+++ tests/evaluation_tests.rs
@@ -0,0 +1,19 @@
+use quint_simulator::{
+    evaluator::run,
+    ir::{QuintDef, QuintOutput},
+};
+use std::fs::File;
+
+#[test]
+fn simple() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("fixtures/simple.json").unwrap();
+    let parsed: QuintOutput = serde_json::from_reader(file).unwrap();
+
+    // Evaluate the expression inside the second declaration
+    if let QuintDef::QuintOpDef(def) = &parsed.modules[0].declarations[1] {
+        let value = run(&parsed.table, &def.expr)?;
+        assert_eq!(value.to_string(), "2");
+    };
+
+    Ok(())
+}
