cab6ac563d15b66a8f34c467ed4844be146d7014
bugarela <gabrielamoreira05@gmail.com>
2025-02-04 15:35:26 -0300

    Delete other evaluation tests

--- tests/evaluation_tests.rs
+++ tests/evaluation_tests.rs
@@ -1,56 +1,10 @@
 use quint_simulator::{
     evaluator::run,
     ir::{QuintDef, QuintOutput},
-    value::Value,
 };
-use std::fs::File;
 use std::io::Write;
 use std::process::Command;
 use tempfile::NamedTempFile;
-
-#[test]
-fn simple() -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("fixtures/simple.json").unwrap();
-    let parsed: QuintOutput = serde_json::from_reader(file).unwrap();
-
-    // Evaluate the expression inside the second declaration
-    if let QuintDef::QuintOpDef(def) = &parsed.modules[0].declarations[1] {
-        let value = run(&parsed.table, &def.expr).unwrap();
-        assert_eq!(value.to_string(), "2");
-    };
-
-    Ok(())
-}
-
-#[test]
-fn ints_and_sets() -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("fixtures/ints_and_sets.json").unwrap();
-    let parsed: QuintOutput = serde_json::from_reader(file).unwrap();
-
-    // Evaluate the expression inside the second declaration
-    if let QuintDef::QuintOpDef(def) = &parsed.modules[0].declarations[3] {
-        let value = run(&parsed.table, &def.expr).unwrap();
-        assert_eq!(
-            value,
-            Value::Set(vec![4, 6, 7, 8].into_iter().map(Value::Int).collect())
-        );
-    };
-
-    Ok(())
-}
-#[test]
-fn sets() -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("fixtures/sets.json").unwrap();
-    let parsed: QuintOutput = serde_json::from_reader(file).unwrap();
-
-    // Evaluate the expression inside the second declaration
-    if let QuintDef::QuintOpDef(def) = &parsed.modules[0].declarations[6] {
-        let value = run(&parsed.table, &def.expr).unwrap();
-        assert_eq!(value, Value::Bool(true));
-    };
-
-    Ok(())
-}
 
 fn assert_from_string(input: &str, expected: &str) -> Result<(), Box<dyn std::error::Error>> {
     let quint_content = format!(
