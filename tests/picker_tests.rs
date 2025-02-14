4ebe0fd66f96767ab9ab05da713c1e2b78a83a42
Gabriela Moreira <gabrielamoreira05@gmail.com>
2025-02-14 15:52:25 -0300

    Add test for picking from interval

--- tests/picker_tests.rs
+++ tests/picker_tests.rs
@@ -5,6 +5,30 @@ use quint_simulator::{
 
 mod helpers;
 
+macro_rules! run_test {
+    ($content:expr, $expected_value:expr) => {{
+        let parsed = helpers::parse($content)?;
+        let init_def = helpers::find_definition_by_name(&parsed, "init")?;
+
+        let mut interpreter = Interpreter::new(&parsed.table);
+        let mut env = Env::new(interpreter.var_storage.clone());
+
+        // Set a specific seed so different runs generate the same result
+        env.rand.set_state(123_456);
+
+        let init = interpreter.eval(&mut env, &init_def.expr);
+        assert_eq!(init.unwrap(), Value::Bool(true));
+
+        interpreter.shift();
+
+        let input_def = helpers::find_definition_by_name(&parsed, "input")?;
+        let input = interpreter.eval(&mut env, &input_def.expr);
+        assert_eq!(input.unwrap(), $expected_value);
+
+        Ok(())
+    }};
+}
+
 #[test]
 fn set_pick_test() -> Result<(), Box<dyn std::error::Error>> {
     let quint_content = "module main {
@@ -17,24 +41,20 @@ fn set_pick_test() -> Result<(), Box<dyn std::error::Error>> {
           action step = x' = x
         }";
 
-    let parsed = helpers::parse(quint_content)?;
-    let init_def = helpers::find_definition_by_name(&parsed, "init")?;
-    let input_def = helpers::find_definition_by_name(&parsed, "input")?;
+    run_test!(quint_content, Value::Int(2))
+}
 
-    let mut interpreter = Interpreter::new(&parsed.table);
-    let mut env = Env::new(interpreter.var_storage.clone());
+#[test]
+fn interval_pick_test() -> Result<(), Box<dyn std::error::Error>> {
+    let quint_content = "module main {
+          var x: int
+          val input = x 
+          action init = {
+            nondet v = 5.to(8).oneOf()
+            x' = v
+          }
+          action step = x' = x
+        }";
 
-    // Set a specific seed so different runs generate the same result
-    env.rand.set_state(123_456);
-
-    let init = interpreter.eval(&mut env, &init_def.expr);
-    assert_eq!(init.unwrap(), Value::Bool(true));
-
-    interpreter.shift();
-
-    // After shifting, the value of x should be 2
-    let input = interpreter.eval(&mut env, &input_def.expr);
-    assert_eq!(input.unwrap(), Value::Int(2));
-
-    Ok(())
+    run_test!(quint_content, Value::Int(5))
 }
