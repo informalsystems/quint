c978c502d9261bd24393e2c981ffa0d60e519b14
Gabriela Moreira <gabrielamoreira05@gmail.com>
2025-02-17 18:49:25 -0300

    Add tests

---
+++ tests/simulator_tests.rs
@@ -0,0 +1,27 @@
+use std::path::Path;
+
+use quint_simulator::helpers;
+
+#[test]
+fn tictactoe_ok() {
+    let file_path: &Path = Path::new("fixtures/tictactoe.qnt");
+
+    let parsed = helpers::parse_from_path(file_path).unwrap();
+    // Pass an invariant that should hold
+    let result = parsed.simulate("init", "step", "inv", 10, 100);
+    assert!(result.is_ok());
+    // Should not find violation
+    assert!(result.unwrap().result);
+}
+
+#[test]
+fn tictactoe_violation() {
+    let file_path: &Path = Path::new("fixtures/tictactoe.qnt");
+
+    let parsed = helpers::parse_from_path(file_path).unwrap();
+    // Pass an invariant that should not hold
+    let result = parsed.simulate("init", "step", "XHasNotWon", 10, 100);
+    assert!(result.is_ok());
+    // Should find violation
+    assert!(!result.unwrap().result);
+}
