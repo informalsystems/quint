18a851706ba7d22fdbffb43320ced90318ace9ff
Romain Ruetschi <romain@informal.systems>
2025-02-18 13:31:01 +0100

    Do 1000 steps on CI instead of 100
    Co-authored-by: Gabriela Moreira <gabrielamoreira05@gmail.com>

--- benches/simulation.rs
+++ benches/simulation.rs
@@ -8,7 +8,7 @@ use quint_simulator::helpers;
 
 fn run_in_rust(file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let parsed = helpers::parse_from_path(file_path)?;
-    let result = parsed.simulate("init", "step", "inv", 10, 100);
+    let result = parsed.simulate("init", "step", "inv", 10, 1_000);
 
     match result {
         Ok(r) => assert!(r.result),
@@ -21,7 +21,7 @@ fn run_in_rust(file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
 fn run_in_quint(file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let quint = Command::new("quint")
         .arg("run")
-        .args(["--max-samples", "100"])
+        .args(["--max-samples", "1000"])
         .args(["--max-steps", "10"])
         .arg(file_path)
         .stdout(Stdio::piped())
