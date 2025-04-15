e6c9dcf4444f6472c8bc39512c018734f5051140
Gabriela Moreira <gabrielamoreira05@gmail.com>
2025-04-15 11:17:06 -0300

    Fix problem with missing constants being cached

--- evaluator/tests/simulator_tests.rs
+++ evaluator/tests/simulator_tests.rs
@@ -41,3 +41,18 @@ fn instances_ok() {
     // Should not find violation
     assert!(result.unwrap().result);
 }
+
+#[test]
+/// inv2 of the same spec ensures constants and values referencing them are compiled correctly
+fn instance_overrides_ok() {
+    let file_path: &Path = Path::new("fixtures/instances.qnt");
+
+    let parsed =
+        helpers::parse_from_path(file_path, "init", "step", Some("inv2"), Some("instances"))
+            .unwrap();
+    // Pass an invariant that should hold
+    let result = parsed.simulate(10, 100, 0);
+    assert!(result.is_ok());
+    // Should not find violation
+    assert!(result.unwrap().result);
+}
