ce1bb203e444c7f80c518e40eed4ce8e27b4dadc
Georgine Forner <jackson.richard.btc@gmail.com>
2025-05-20 01:09:38 +0600

    refactor: update simulate function calls to include optional parameter

--- evaluator/tests/simulator_tests.rs
+++ evaluator/tests/simulator_tests.rs
@@ -8,7 +8,7 @@ fn tictactoe_ok() {
 
     let parsed = helpers::parse_from_path(file_path, "init", "step", Some("inv"), None).unwrap();
     // Pass an invariant that should hold
-    let result = parsed.simulate(10, 100, 0);
+    let result = parsed.simulate(10, 100, 0, None);
     assert!(result.is_ok());
     // Should not find violation
     assert!(result.unwrap().result);
@@ -18,10 +18,10 @@ fn tictactoe_ok() {
 fn tictactoe_violation() {
     let file_path: &Path = Path::new("fixtures/tictactoe.qnt");
 
-    let parsed =
-        helpers::parse_from_path(file_path, "init", "step", Some("XHasNotWon"), None).unwrap();
+    let parsed = helpers::parse_from_path(file_path, "init", "step", Some("XHasNotWon"), None)
+        .unwrap();
     // Pass an invariant that should not hold
-    let result = parsed.simulate(10, 100, 0);
+    let result = parsed.simulate(10, 100, 0, None);
     assert!(result.is_ok());
     // Should find violation
     assert!(!result.unwrap().result);
@@ -32,11 +32,10 @@ fn tictactoe_violation() {
 fn instances_ok() {
     let file_path: &Path = Path::new("fixtures/instances.qnt");
 
-    let parsed =
-        helpers::parse_from_path(file_path, "init", "step", Some("inv"), Some("instances"))
-            .unwrap();
+    let parsed = helpers::parse_from_path(file_path, "init", "step", Some("inv"), Some("instances"))
+        .unwrap();
     // Pass an invariant that should hold
-    let result = parsed.simulate(10, 100, 0);
+    let result = parsed.simulate(10, 100, 0, None);
     assert!(result.is_ok());
     // Should not find violation
     assert!(result.unwrap().result);
@@ -47,11 +46,10 @@ fn instances_ok() {
 fn instance_overrides_ok() {
     let file_path: &Path = Path::new("fixtures/instances.qnt");
 
-    let parsed =
-        helpers::parse_from_path(file_path, "init", "step", Some("inv2"), Some("instances"))
-            .unwrap();
+    let parsed = helpers::parse_from_path(file_path, "init", "step", Some("inv2"), Some("instances"))
+        .unwrap();
     // Pass an invariant that should hold
-    let result = parsed.simulate(10, 100, 0);
+    let result = parsed.simulate(10, 100, 0, None);
     assert!(result.is_ok());
     // Should not find violation
     assert!(result.unwrap().result);
