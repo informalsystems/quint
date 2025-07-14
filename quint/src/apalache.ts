0f23abb034e4be7abe5875e8f75611c2b445e3d7
Gabriela Moreira <gabrielamoreira05@gmail.com>
2025-07-14 13:27:55 -0300

    Improve error handling to match unqualified variables as well

--- quint/src/apalache.ts
+++ quint/src/apalache.ts
@@ -171,10 +171,10 @@ async function handleResponse(response: RunResponse): Promise<ApalacheResult<any
 
         // Check for the specific assignment error pattern
         const assignmentErrorMatch = errData.msg.match(
-          /Assignment error: <\[UNKNOWN\]>: (\w+)::\w+::(\w+)' is used before it is assigned/
+          /Assignment error: <\[UNKNOWN\]>: (?:\w+::)*(\w+)' is used before it is assigned/
         )
         if (assignmentErrorMatch) {
-          const [, , variableName] = assignmentErrorMatch
+          const [, variableName] = assignmentErrorMatch
           return err(
             `${variableName} is used before it is assigned. You need to have either \`${variableName} == <expr>\` or \`${variableName}.in(<set>)\` before doing anything else with \`${variableName}\` in your predicate.`
           )
