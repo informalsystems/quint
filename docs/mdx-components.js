7a521168fbda78f8c4502bd8f89b8ed713d4f610
Gabriela Moreira <gabrielamoreira05@gmail.com>
2025-07-21 15:52:24 -0300

    Delete old file and comments

--- docs/mdx-components.js
+++ docs/mdx-components.js
@@ -1,15 +1,12 @@
-import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
-// import { useMDXComponents as getBlogMDXComponents } from 'nextra-theme-blog'
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
 
 // Get the default MDX components
 const themeComponents = getThemeComponents()
-// const blogComponents = getBlogMDXComponents()
 
 // Merge components
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
-    // ...blogComponents,
     ...components
   }
 }
