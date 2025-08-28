f451185e001495cd954214cc68857efdd185b746
Gabriela Moreira <gabrielamoreira05@gmail.com>
2025-08-28 14:36:56 -0300

    Fix formatting

--- quint/src/quintAnalyzer.ts
+++ quint/src/quintAnalyzer.ts
@@ -99,7 +99,7 @@ class QuintAnalyzer {
     this.typeApplicationResolver = new TypeApplicationResolver(lookupTable)
     this.typeInferrer = new TypeInferrer(lookupTable, previousOutput?.types)
     /// FIXES: https://github.com/informalsystems/quint/issues/428
-    this.effectInferrer = new EffectInferrer(lookupTable, new Map([...previousOutput?.effects.entries() ?? []]))
+    this.effectInferrer = new EffectInferrer(lookupTable, new Map([...(previousOutput?.effects.entries() ?? [])]))
     this.multipleUpdatesChecker = new MultipleUpdatesChecker()
     this.modeChecker = new ModeChecker(previousOutput?.modes)
     this.nondetChecker = new NondetChecker(lookupTable)
