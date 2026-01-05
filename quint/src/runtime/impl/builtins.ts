ae15e067e0b704694f0e5fa66734159aa54f74bc
Gabriela Moreira <gabrielamoreira05@gmail.com>
2026-01-05 16:36:56 -0300

    Prevent `expect` from adding a state to the trace

--- quint/src/runtime/impl/builtins.ts
+++ quint/src/runtime/impl/builtins.ts
@@ -302,6 +302,7 @@ export function lazyBuiltinLambda(
           ctx.shift()
           return args[1](ctx).chain(expectation => {
             ctx.varStorage.recoverSnapshot(nextVarsSnapshot)
+            ctx.trace.dropLast()
 
             if (!expectation.toBool()) {
               return left({ code: 'QNT508', message: 'Expect condition does not hold true' })
