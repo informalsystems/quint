d156b73c85fc3dad6f5ca1b662dc5e0bd870f309
Gabriela Moreira <gabrielamoreira05@gmail.com>
2026-01-14 16:44:48 -0300

    Fix let caching

--- evaluator/src/evaluator.rs
+++ evaluator/src/evaluator.rs
@@ -573,8 +573,13 @@ impl<'a> Interpreter<'a> {
                     };
                     let compiled_expr = self.compile(expr);
                     CompiledExpr::new(move |env| {
-                        let result = compiled_expr.execute(env);
+                        let saved = cached_value.borrow().clone();
+
                         cached_value.replace(None);
+                        let result = compiled_expr.execute(env);
+
+                        cached_value.replace(saved);
+
                         result
                     })
                 }
