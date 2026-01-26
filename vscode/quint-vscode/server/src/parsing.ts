28fb5f282b32bf37be1318791d7d062feb2f2b43
Erick Pintor <erickpintor@gmail.com>
2026-01-26 16:26:47 -0300

    Introduce async compilation pipeline to LSP server
    Quint compilation process is not incremental or async. Due to its single
    threaded model, the LSP server can freeze on some requests while
    compiling large modules.
    
    This patch pushes compilation and code analysis to a separate worker
    thread so that the LSP server can continue to respond to LSP client
    requests while the code is being compiled and analyzed in the
    background.
    
    The trade-off here is that on large modules we may potentially delay
    showing compilation errors or respond to request, like goto definition,
    based on a stale version of the module while the new one is being
    compiled.

--- vscode/quint-vscode/server/src/parsing.ts
+++ vscode/quint-vscode/server/src/parsing.ts
@@ -9,33 +9,18 @@ import {
 } from '@informalsystems/quint'
 import { basename, dirname } from 'path'
 import { URI } from 'vscode-uri'
-import { TextDocument } from 'vscode-languageserver-textdocument'
 import { flow } from 'lodash'
 
-export async function parseDocument(idGenerator: IdGenerator, textDocument: TextDocument): Promise<ParserPhase4> {
-  const text = textDocument.getText()
-
-  // parse the URI to resolve imports
-  const parsedUri = URI.parse(textDocument.uri)
-  // currently, we only support the 'file://' scheme
-  if (parsedUri.scheme !== 'file') {
-    // see https://github.com/informalsystems/quint/issues/831
-    return new Promise((_resolve, reject) => reject(`Support imports from file, found: ${parsedUri.scheme}`))
-  }
-  try {
-    const result: ParserPhase4 = flow([
-      () => parsePhase1fromText(idGenerator, text, parsedUri.path),
-      phase1Data => {
-        const resolver = fileSourceResolver()
-        const mainPath = resolver.lookupPath(dirname(parsedUri.fsPath), basename(parsedUri.fsPath))
-        return parsePhase2sourceResolution(idGenerator, resolver, mainPath, phase1Data)
-      },
-      parsePhase3importAndNameResolution,
-      parsePhase4toposort,
-    ])()
-
-    return new Promise((resolve, _reject) => resolve(result))
-  } catch (e) {
-    return new Promise((_resolve, reject) => reject(e))
-  }
+export function parseDocument(idGenerator: IdGenerator, uri: URI, text: string): ParserPhase4 {
+  const result: ParserPhase4 = flow([
+    () => parsePhase1fromText(idGenerator, text, uri.path),
+    phase1Data => {
+      const resolver = fileSourceResolver()
+      const mainPath = resolver.lookupPath(dirname(uri.fsPath), basename(uri.fsPath))
+      return parsePhase2sourceResolution(idGenerator, resolver, mainPath, phase1Data)
+    },
+    parsePhase3importAndNameResolution,
+    parsePhase4toposort,
+  ])()
+  return result
 }
