4b91c5796723442f2bf2965c28ae0a7d5bc60ad3
beu5a <boukhari.yassine@pm.me>
2026-02-20 10:37:24 +0100

    add new line to reporter stderr output

--- evaluator/src/progress.rs
+++ evaluator/src/progress.rs
@@ -54,7 +54,8 @@ impl Reporter for JsonStdErr {
     fn next_sample(&mut self) {
         self.current_samples += 1;
         let progress = Self::fmt(self.current_samples, self.total_samples);
-        serde_json::to_writer(io::stderr(), &progress).expect("failed to write progress to stdout");
+        let mut stderr = io::stderr().lock();
+        writeln!(stderr, "{progress}").expect("failed to write progress to stderr");
     }
 }
 
