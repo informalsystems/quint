5dab79bc734072242dd9fadd390e007be3d88f0b
nikola-informal <nikolam@informal.systems>
2026-04-06 18:06:31 +0200

    feat: replace images

--- docs/components/home/AuditsSection.tsx
+++ docs/components/home/AuditsSection.tsx
@@ -6,8 +6,8 @@ import { LinkButton } from '../LinkButton'
 export const AuditsSection = () => {
   return (
     <>
-      <section className="relative overflow-visible bg-quint-purple/10">
-        <div className="mx-auto max-w-7xl px-6 pt-25 pb-25">
+      <section className="relative bg-[url('/bg-audits.png')] bg-cover bg-center bg-no-repeat">
+        <div className="mx-auto max-w-7xl px-6 pt-30 pb-30">
           <h2 className="text-4xl sm:text-5xl md:text-[54px] leading-tight font-semibold font-[family-name:var(--font-instrument-sans)]">
             <span className="text-quint-dark">We don&apos;t just build Quint. </span>
             <span className="text-quint-purple">We use it.</span>
